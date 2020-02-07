85410ced5cff990aef16556a051cc77e3dc875b1
Chad Baker <chad.baker@nrel.gov>
2020-02-07 10:19:51 -0700

    made initSoc in SimDriveClassic work like initSoc in SimDriveJit

--- src/SimDrive.py
+++ src/SimDrive.py
@@ -913,7 +913,7 @@ class SimDriveClassic(SimDriveCore):
 
             # If no EV / Hybrid components, no SOC considerations.
 
-            initSoc = 0.0
+            initSoc = (veh.maxSoc + veh.minSoc) / 2.0
 
             self.sim_drive_sub(cyc, veh, initSoc)
 
