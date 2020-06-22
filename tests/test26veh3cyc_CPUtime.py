17ee1099e4ed29ef0b87e024654be969157e9126
Chad Baker <chad.baker@nrel.gov>
2020-06-22 10:26:35 -0600

    moved contents into function that can either run in __main__ or as import

--- tests/test26veh3cyc_CPUtime.py
+++ tests/test26veh3cyc_CPUtime.py
@@ -17,35 +17,41 @@ if fsimpath not in sys.path:
 from fastsim import simdrive, vehicle, cycle
 importlib.reload(simdrive)
 
-t0 = time.time()
+def run_test26veh3cyc_CPUtime(use_jitclass=True):
+    t0 = time.time()
 
-cycles = ['udds', 'hwfet', 'us06']
-vehicles = np.arange(1, 27)
+    cycles = ['udds', 'hwfet', 'us06']
+    vehicles = np.arange(1, 27)
 
-print('Instantiating classes.')
-print()
-veh = vehicle.Vehicle(1)
-veh_jit = veh.get_numba_veh()
-cyc = cycle.Cycle('udds')
-cyc_jit = cyc.get_numba_cyc()
+    print('Instantiating classes.')
+    print()
+    veh = vehicle.Vehicle(1)
+    veh_jit = veh.get_numba_veh()
+    cyc = cycle.Cycle('udds')
+    cyc_jit = cyc.get_numba_cyc()
 
-iter = 0
-for vehno in vehicles:
-    print('vehno =', vehno)
-    if vehno == 2:
-        t0a = time.time()
-    for cycname in cycles:
-        if not((vehno == 1) and (cycname == 'udds')):
-            cyc.set_standard_cycle(cycname)
-            cyc_jit = cyc.get_numba_cyc()
-            veh.load_veh(vehno)
-            veh_jit = veh.get_numba_veh()
-        sim_drive = simdrive.SimDriveJit(cyc_jit, veh_jit)
-        # sim_drive = simdrive.SimDriveClassic(cyc, veh)
-        sim_drive.sim_drive(-1)
-        # sim_drive.sim_drive()
+    iter = 0
+    for vehno in vehicles:
+        print('vehno =', vehno)
+        if vehno == 2:
+            t0a = time.time()
+        for cycname in cycles:
+            if not((vehno == 1) and (cycname == 'udds')):
+                cyc.set_standard_cycle(cycname)
+                cyc_jit = cyc.get_numba_cyc()
+                veh.load_veh(vehno)
+                veh_jit = veh.get_numba_veh()
+                if use_jitclass:
+                    sim_drive = simdrive.SimDriveJit(cyc_jit, veh_jit)
+                    sim_drive.sim_drive(-1)
+                else:
+                    sim_drive = simdrive.SimDriveClassic(cyc_jit, veh_jit)
+                    sim_drive.sim_drive()
 
-t1 = time.time()
-print()
-print('Elapsed time: ', round(t1 - t0, 2), 's')
-print('Elapsed time since first vehicle: ', round(t1 - t0a, 2), 's')
+    t1 = time.time()
+    print()
+    print('Elapsed time: ', round(t1 - t0, 2), 's')
+    print('Elapsed time since first vehicle: ', round(t1 - t0a, 2), 's')
+
+if __name__ == "__main__":
+    run_test26veh3cyc_CPUtime()
