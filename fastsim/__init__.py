3c6fe0f8beb7db7656b2ad97b58caa5df9b74456
Chad Baker <chad.baker@nrel.gov>
2020-07-02 13:38:08 -0600

    added imports to get desired behavior when using `import fastsim.cycle`

--- fastsim/__init__.py
+++ fastsim/__init__.py
@@ -1,2 +1,7 @@
 """Package contaning modules for running FASTSim.  
 For example usage, see ../README.md"""
+
+from fastsim import cycle
+from fastsim import vehicle
+from fastsim import simdrive
+from fastsim import globalvars as gl
