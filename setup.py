56f8d0ff6f45232c638c6533449351e567e65325
Chad Baker <chad.baker@nrel.gov>
2020-09-03 13:40:30 -0600

    stole LICENSE and setup.py from hot_oop.  might be able to pip install now

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fastsim",  # Replace with your own username
+    version="0.0.1",
+    author="MBAP Group",
+    author_email="fastsim@nrel.gov",
+    description="Tool for modeling vehicle powertrains",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.nrel.gov/transportation/fastsim.html",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "pandas>=1",
+        "matplotlib",
+        "numpy>=1.18",
+        "numba>=0.50",
+        "scipy>=1.5",
+        "seaborn>=0.10",
+    ],
+)
