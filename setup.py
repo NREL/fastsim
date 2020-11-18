fc4dbf08f17834ca2df375aabcedb661f8fb6eb3
Chad Baker <chad.baker@nrel.gov>
2020-11-18 13:50:14 -0700

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fastsim",  # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="MBAP Group",
     author_email="fastsim@nrel.gov",
     description="Tool for modeling vehicle powertrains",
