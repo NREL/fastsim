4bdb685758f4774b053999102ec31b3de020cf06
Chad Baker <chad.baker@nrel.gov>
2020-12-14 11:07:04 -0700

    removed unneeded imports in __init__.py

--- fastsim/__init__.py
+++ fastsim/__init__.py
@@ -1,9 +1,6 @@
 """Package contaning modules for running FASTSim.  
 For example usage, see ../README.md"""
 
-from fastsim import cycle
-from fastsim import vehicle
-from fastsim import simdrive
 from fastsim import parameters as params
 from fastsim import utilities as utils
 
