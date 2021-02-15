927ba0928000f5afc0265722945cf6d78d2470a2
Chad Baker <chad.baker@nrel.gov>
2021-02-15 15:57:02 -0700

    reordered imports to allow usage in simdrivehot

--- fastsim/__init__.py
+++ fastsim/__init__.py
@@ -1,10 +1,10 @@
 """Package contaning modules for running FASTSim.  
 For example usage, see ../README.md"""
 
-from fastsim import simdrive, vehicle, cycle, simdrivehot, tests
 # convenient aliases
 from fastsim import utilities as utils
 from fastsim import parameters as params
+from fastsim import simdrive, vehicle, cycle, simdrivehot, tests
 
 from pkg_resources import get_distribution
 
