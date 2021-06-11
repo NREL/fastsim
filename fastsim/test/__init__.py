9db3feaa395437acfdcdc113de343a436521350e
Chad Baker <chad.baker@nrel.gov>
2021-06-11 16:02:01 -0600

    more descriptive function name

--- fastsim/test/__init__.py
+++ fastsim/test/__init__.py
@@ -6,7 +6,7 @@ from . import test_simdrive_sweep, test_vs_excel
 # from . import test_vs_excel # this test does not work yet and is therefore not imported for now
 # from . import test_time_dilation # not setup to be used as module so not imported
 
-def run_all_working_tests(use_jitclass=True):
+def run_functional_tests(use_jitclass=True):
     """Runs all tests that are currently functioning as modules.
     Tests not run by this function are functioning as scripts --
     except test_vs_excel.py, which has known bugs."""
