2e0e9501f9edf4c4e2550bf1ef054471495f7920
Chad Baker <chad.baker@nrel.gov>
2021-06-23 14:19:00 -0600

    first test for simdrivehot

---
+++ fastsim/test/test_simdrivehot.py
@@ -0,0 +1,37 @@
+"""Test suite for simdrivehot instantiation and usage."""
+
+import unittest
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+from fastsim import cycle, vehicle, simdrivehot
+
+
+def get_fc_temp_delta():
+    """Returns FC/engine temperature delta over UDDS cycle."""
+    cyc = cycle.Cycle('udds')
+    veh = vehicle.Vehicle(1)
+    sim_drive = simdrivehot.SimDriveHot(
+        cyc, veh,
+        teAmbDegC=np.ones(len(cyc.cycSecs)) * 22,
+        teFcInitDegC=22)
+    sim_drive.sim_drive()
+    delta = sim_drive.teFcDegC[-1] - sim_drive.teFcDegC[0]
+    return delta
+
+
+class TestSimDriveHot(unittest.TestCase):
+    def test_fc_temperature(self):
+        """Test to verify expected delta between starting temperature and final temperature."""
+        # value for running above vehicle and cycle combination as of git commit
+        # 27b99823dfd1
+        reference_value = 65.68819480855775
+
+        self.assertEqual(
+            reference_value, 
+            get_fc_temp_delta()
+        )
+
+if __name__ == '__main__':
+    unittest.main()
