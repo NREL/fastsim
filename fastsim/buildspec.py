f04b85c057d3d39aa62244e85e77ba4b9fe3cf08
Chad Baker <chad.baker@nrel.gov>
2021-09-29 14:13:50 -0600

    removed deprecated comment

--- fastsim/buildspec.py
+++ fastsim/buildspec.py
@@ -68,9 +68,7 @@ def build_spec(instance, error='raise', extra=None):
 
     def isprop(attr):
         return isinstance(attr, property)
-
-    # TODO, low urgency: this eventually needs needs to be more general.  The easiest way to do this
-    # is to append this list with the same list for cycle.Cycle and other possible classes 
+        
     prop_attrs = [name for (name, _) in inspect.getmembers(type(instance), isprop)]
 
     for key, val in instance.__dict__.items():
