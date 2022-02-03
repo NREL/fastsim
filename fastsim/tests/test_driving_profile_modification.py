1cc9bfd5e517549fc4a9970afa36fbb27ebffece
Michael O'Keefe <Michael.OKeefe@nrel.gov>
2022-02-03 08:48:03 -0700

    Add stub for test

---
+++ fastsim/tests/test_driving_profile_modification.py
@@ -0,0 +1,18 @@
+"""
+Tests that check the drive cycle modification functionality.
+"""
+import unittest
+
+import fastsim
+
+class TestDrivingProfileModification(unittest.TestCase):
+    def setUp(self) -> None:
+        # create a trapezoidal trip shape
+
+        return super().setUp()
+    
+    def tearDown(self) -> None:
+        return super().tearDown()
+    
+    def test_that_eco_approach_engages():
+        pass
