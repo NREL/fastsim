dc7649949475cb7d4af3d77909a4e964291d0f84
Chad Baker <chad.baker@nrel.gov>
2022-02-11 16:18:03 -0700

    fixed bad version req

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: Other/Proprietary License",
         "Operating System :: OS Independent",
     ],
-    python_requires='3.8',
+    python_requires='>=3.7,<3.9',
     include_package_data=True,
     package_data={
         "fastsim.resources": ["*"],
