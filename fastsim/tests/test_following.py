faaeaca1251f1c577d02909a3cc45a0a2c80b241
Michael O'Keefe <mokeefe@nrel.gov>
2022-02-28 11:17:04 -0700

    Add exploration plots of IDM parameters

--- fastsim/tests/test_following.py
+++ fastsim/tests/test_following.py
@@ -223,74 +223,123 @@ class TestFollowing(unittest.TestCase):
             import matplotlib.pyplot as plt
             import pandas as pd
             import seaborn as sns
+            import pathlib as pl
+            datafile_name = 'test_sweeping_idm_parameters.csv'
             sns.set()
 
-            udds = fastsim.cycle.Cycle('udds').get_numba_cyc()
-            veh = fastsim.vehicle.Vehicle(5, verbose=False).get_numba_veh()
-            average_speed_m__s = np.mean(udds.cycMps)
-            average_speed_when_moving_m__s = np.mean(udds.cycMps[udds.cycDistMeters_v2 > 0.0])
-            max_speed_m__s = np.max(udds.cycMps)
-            dt_headways = np.linspace(0.5, 4.0, num=8, endpoint=True)
-            v_desireds = np.array(sorted([average_speed_m__s, average_speed_when_moving_m__s, max_speed_m__s]))
-            s_mins = np.linspace(1.0, 10.0, num=10, endpoint=True)
-            deltas = np.linspace(2.0, 6.0, num=5, endpoint=True)
-            a_accels = np.linspace(0.5, 2.5, num=5, endpoint=True)
-            b_accels = np.linspace(1.0, 3.0, num=5, endpoint=True)
-            sd = fastsim.simdrive.SimDriveJit(udds, veh)
-            sd.sim_drive()
-            base_distance_m = sd.cyc.cycDistMeters_v2.sum()
-            base_fuel_consumption_gal__100mi = 100.0 / sd.mpgge
-            results = {
-                'dt_headway_s': [0.0],
-                'v_desired_m__s': [0.0],
-                's_min_m': [0.0],
-                'delta': [0.0],
-                'a_m__s2': [0.0],
-                'b_m__s2': [0.0],
-                'fuel_economy_mpgge': [sd.mpgge],
-                'fuel_consumption_gal__100mi': [base_fuel_consumption_gal__100mi],
-                'distance_m': [base_distance_m],
-                'distance_short_m': [0.0],
-            }
-            idx = 0
-            for dt_h_s in dt_headways:
-                for v_d_m__s in v_desireds:
-                    for s_min_m in s_mins:
-                        for delta in deltas:
-                            for a_m__s2 in a_accels:
-                                for b_m__s2 in b_accels:
-                                    idx += 1
-                                    if idx % 10 == 0:
-                                        print(f"Running {idx}...")
-                                    if True:
-                                        veh.lead_accel_coef_s2 = 0.0
-                                        veh.lead_speed_coef_s = 0.0
-                                        veh.lead_offset_m = 0.0
-                                        veh.idm_minimum_gap_m = s_min_m
-                                        veh.idm_delta = delta
-                                        veh.idm_accel_m__s2 = a_m__s2
-                                        veh.idm_decel_m__s2 = b_m__s2
-                                        veh.idm_v_desired_m__s = v_d_m__s
-                                        veh.idm_dt_headway_s = dt_h_s
-                                        sd = fastsim.simdrive.SimDriveJit(udds, veh)
-                                        sd.sim_params.follow_allow = True
-                                        sd.sim_params.follow_model = fastsim.simdrive.FOLLOW_MODEL_IDM
-                                        sd.sim_drive()
-                                        results['dt_headway_s'].append(dt_h_s)
-                                        results['v_desired_m__s'].append(v_d_m__s)
-                                        results['s_min_m'].append(s_min_m)
-                                        results['delta'].append(delta)
-                                        results['a_m__s2'].append(a_m__s2)
-                                        results['b_m__s2'].append(b_m__s2)
-                                        results['fuel_economy_mpgge'].append(sd.mpgge)
-                                        if sd.mpgge > 0.0:
-                                            results['fuel_consumption_gal__100mi'].append(100.0 / sd.mpgge)
-                                        else:
-                                            results['fuel_consumption_gal__100mi'].append(-1.0)
-                                        dist_m = sd.cyc.cycDistMeters_v2.sum()
-                                        results['distance_m'].append(dist_m)
-                                        results['distance_short_m'].append(base_distance_m - dist_m)
-            print(f"idx: {idx}")
-            results = pd.DataFrame(results)
-            results.to_csv('test_sweeping_idm_parameters.csv')
+            results = None
+            if not pl.Path(datafile_name).exists():
+                udds = fastsim.cycle.Cycle('udds').get_numba_cyc()
+                veh = fastsim.vehicle.Vehicle(5, verbose=False).get_numba_veh()
+                average_speed_m__s = np.mean(udds.cycMps)
+                average_speed_when_moving_m__s = np.mean(udds.cycMps[udds.cycDistMeters_v2 > 0.0])
+                max_speed_m__s = np.max(udds.cycMps)
+                dt_headways = np.linspace(0.5, 4.0, num=8, endpoint=True)
+                v_desireds = np.array(sorted([average_speed_m__s, average_speed_when_moving_m__s, max_speed_m__s]))
+                s_mins = np.linspace(1.0, 10.0, num=10, endpoint=True)
+                deltas = np.linspace(2.0, 6.0, num=5, endpoint=True)
+                a_accels = np.linspace(0.5, 2.5, num=5, endpoint=True)
+                b_accels = np.linspace(1.0, 3.0, num=5, endpoint=True)
+                sd = fastsim.simdrive.SimDriveJit(udds, veh)
+                sd.sim_drive()
+                base_distance_m = sd.cyc.cycDistMeters_v2.sum()
+                base_fuel_consumption_gal__100mi = 100.0 / sd.mpgge
+                results = {
+                    'dt_headway_s': [0.0],
+                    'v_desired_m__s': [0.0],
+                    's_min_m': [0.0],
+                    'delta': [0.0],
+                    'a_m__s2': [0.0],
+                    'b_m__s2': [0.0],
+                    'fuel_economy_mpgge': [sd.mpgge],
+                    'fuel_consumption_gal__100mi': [base_fuel_consumption_gal__100mi],
+                    'distance_m': [base_distance_m],
+                    'distance_short_m': [0.0],
+                }
+                idx = 0
+                for dt_h_s in dt_headways:
+                    for v_d_m__s in v_desireds:
+                        for s_min_m in s_mins:
+                            for delta in deltas:
+                                for a_m__s2 in a_accels:
+                                    for b_m__s2 in b_accels:
+                                        idx += 1
+                                        if idx % 10 == 0:
+                                            print(f"Running {idx}...")
+                                        if True:
+                                            veh.lead_accel_coef_s2 = 0.0
+                                            veh.lead_speed_coef_s = 0.0
+                                            veh.lead_offset_m = 0.0
+                                            veh.idm_minimum_gap_m = s_min_m
+                                            veh.idm_delta = delta
+                                            veh.idm_accel_m__s2 = a_m__s2
+                                            veh.idm_decel_m__s2 = b_m__s2
+                                            veh.idm_v_desired_m__s = v_d_m__s
+                                            veh.idm_dt_headway_s = dt_h_s
+                                            sd = fastsim.simdrive.SimDriveJit(udds, veh)
+                                            sd.sim_params.follow_allow = True
+                                            sd.sim_params.follow_model = fastsim.simdrive.FOLLOW_MODEL_IDM
+                                            sd.sim_drive()
+                                            results['dt_headway_s'].append(dt_h_s)
+                                            results['v_desired_m__s'].append(v_d_m__s)
+                                            results['s_min_m'].append(s_min_m)
+                                            results['delta'].append(delta)
+                                            results['a_m__s2'].append(a_m__s2)
+                                            results['b_m__s2'].append(b_m__s2)
+                                            results['fuel_economy_mpgge'].append(sd.mpgge)
+                                            if sd.mpgge > 0.0:
+                                                results['fuel_consumption_gal__100mi'].append(100.0 / sd.mpgge)
+                                            else:
+                                                results['fuel_consumption_gal__100mi'].append(-1.0)
+                                            dist_m = sd.cyc.cycDistMeters_v2.sum()
+                                            results['distance_m'].append(dist_m)
+                                            results['distance_short_m'].append(base_distance_m - dist_m)
+                print(f"idx: {idx}")
+                results = pd.DataFrame(results)
+                results.to_csv(datafile_name)
+            else:
+                results = pd.read_csv(datafile_name)
+            results['distance_short_pct'] = results['distance_short_m'] * 100.0 / results['distance_m']
+            # seaborn plots
+            self.assertTrue(results is not None)
+            print(f'Baseline Fuel Consumption: {results["fuel_consumption_gal__100mi"][0]} gal/100mi')
+            param_labels = ['dt_headway_s', 'v_desired_m__s', 's_min_m', 'delta', 'a_m__s2', 'b_m__s2']
+            for hue_item in param_labels:
+                g = sns.displot(
+                    data=results,
+                    x="fuel_consumption_gal__100mi",
+                    hue=hue_item,
+                    multiple="stack",
+                )
+                g.ax.plot([results['fuel_consumption_gal__100mi'][0]]*2, [0, 1000], 'r-', linewidth=2)
+                g.savefig(f'test_sweeping_idm_parameters_distplot_{hue_item}.png', dpi=300)
+                plt.close()
+                g = None
+            g = sns.displot(
+                data=results,
+                x='distance_short_pct',
+                multiple='stack'
+            )
+            g.savefig(f'test_sweeping_idm_parameters_distplot_dist_short.png', dpi=300)
+            plt.close()
+            g = None
+            r = results.drop(index=0)
+            g = sns.FacetGrid(r, row="v_desired_m__s", col="dt_headway_s")
+            g.map(sns.histplot, "fuel_consumption_gal__100mi")
+            g.savefig(f'test_sweeping_idm_parameters_matrix_plot.png', dpi=300)
+            plt.close()
+            g = None
+
+            g = sns.PairGrid(r, x_vars=param_labels, y_vars=["fuel_consumption_gal__100mi"])
+            g.map(sns.scatterplot)
+            g.savefig(f'test_sweeping_idm_parameters_pair_plot.png', dpi=300)
+            plt.close()
+            g = None
+
+            g = sns.PairGrid(r, x_vars=param_labels, y_vars=["fuel_consumption_gal__100mi"])
+            g.map(sns.violinplot)
+            g.savefig(f'test_sweeping_idm_parameters_violin.png', dpi=300)
+            plt.close()
+            g = None
+
             print('DONE!')
