d184f101c225af95fe774162d476b1c0ed146563
Chad Baker <chad.baker@nrel.gov>
2022-03-07 13:55:21 -0700

    fixed test

--- fastsim/tests/test_utils.py
+++ fastsim/tests/test_utils.py
@@ -4,5 +4,5 @@ from fastsim import utils
 class test_utils(unittest.TestCase):
     def test_camel_to_snake(self):
         self.assertEqual("camel2_camel2_case", utils.camel_to_snake("camel2_camel2_case"))
-        self.assertEqual("essAEKwOut", utils.camel_to_snake("ess_ae_kw_out"))
-        self.assertEqual("electKwReq4AE", utils.camel_to_snake("elect_kw_req4_ae"))
+        self.assertEqual("ess_ae_kw_out", utils.camel_to_snake("essAEKwOut"))
+        self.assertEqual("elect_kw_req4_ae", utils.camel_to_snake("electKwReq4AE"))
