17f2ca08e9c7a813ffce1e33c1f2f52d8360585b
Michael O'Keefe <Michael.OKeefe@nrel.gov>
2022-03-15 15:16:33 -0600

    Add tests for new np_argmax fn
    Ensure that np_argmax is robust for empty
    arrays. NOTE: this still may not be the
    greatest way to handle an empty array
    as the index that is returned (i.e., 0 for an
    empty array), cannot be used to lookup
    that array. Possibly, we may want to return
    as an Option type?

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -1,5 +1,5 @@
 extern crate ndarray;
-use ndarray::{Array1, array, Axis, s, concatenate}; 
+use ndarray::{Array, Array1, array, Axis, s, concatenate}; 
 use ordered_float::NotNan; // 2.0.0
 
 
@@ -14,8 +14,13 @@ pub fn diff(x:&Array1<f64>) -> Array1<f64>{
 /// Arguments:
 /// arr: Array1<bool> -- array of bools
 /// Returns index of first true value, or last index if all false
+/// if array is of len() == 0, returns 0.
 pub fn np_argmax(arr: &Array1<bool>) -> usize {
-    arr.iter().position(|&x| x).unwrap_or(arr.len() - 1) // unwrap_or allows for default if not found
+    let len = arr.len();
+    if (len == 0) {
+        return 0;
+    }
+    arr.iter().position(|&x| x).unwrap_or(len - 1) // unwrap_or allows for default if not found
 }
 
 /// return max of 2 f64
@@ -59,5 +64,31 @@ mod tests {
     #[test]
     fn test_diff(){
         assert_eq!(diff(&Array1::range(0.0, 3.0, 1.0)), array![0.0, 1.0, 1.0]);
+    }
+
+    #[test]
+    fn test_that_np_argmax_finds_the_right_index_when_one_exists(){
+        let xs: Array1<bool> = Array::from_vec(vec![false, false, true, false, false]);
+        let idx = np_argmax(&xs);
+        let expected_idx: usize = 2;
+        assert_eq!(idx, expected_idx)
+    }
+
+    #[test]
+    fn test_that_np_argmax_yields_last_index_when_nothing_found(){
+        let xs: Array1<bool> = Array::from_vec(vec![false, false, false, false, false]);
+        let idx = np_argmax(&xs);
+        let expected_idx: usize = xs.len() - 1;
+        assert_eq!(idx, expected_idx)
+    }
+
+    #[test]
+    fn test_that_argmax_does_the_right_thing_on_an_empty_array(){
+        let xs: Array1<bool> = Array::from_vec(vec![]);
+        let idx = np_argmax(&xs);
+        // unclear what should happen here; np.argmax throws a ValueError in the case of an empty vector
+        // ... possibly we should return an Option type?
+        let expected_idx: usize = 0; 
+        assert_eq!(idx, expected_idx);
     }
 }
