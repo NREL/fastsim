6450458cbca6c1d4edc6a0094171ad5845244af1
Chad Baker <chad.baker@nrel.gov>
2022-04-01 11:42:09 -0600

    working toward a good distribution method

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fastsim",  
-    version="1.2.6",
+    version="2.0.0",
     author="MBAP Group",
     author_email="fastsim@nrel.gov",
     description="Tool for modeling vehicle powertrains",
