8dcf377ee60a647ef427c2e7c52fc06ed850056b
Kyle Carow <kcarow@nrel.gov>
2022-07-07 10:32:09 -0400

    maybe finished wrapping RustSimDrive methods and Pyo3 API handlers

--- rust/src/thermal.rs
+++ rust/src/thermal.rs
@@ -226,6 +226,35 @@ impl VehicleThermal {
 }
 
 #[add_pyo3_api(
+    /// method for instantiating SimDriveHot
+    #[new]
+    pub fn __new__(filename: &str) -> Self {
+        Self::from_file_parser(filename).unwrap()
+    }
+
+    #[pyo3(name = "gap_to_lead_vehicle_m")]
+    /// Provides the gap-with lead vehicle from start to finish
+    pub fn gap_to_lead_vehicle_m_py(&self) -> PyResult<Vec<f64>> {
+        Ok(self.gap_to_lead_vehicle_m().to_vec())
+    }
+    
+    #[pyo3(name = "sim_drive")]
+    /// Initialize and run sim_drive_walk as appropriate for vehicle attribute vehPtType.
+    /// Arguments
+    /// ------------
+    /// init_soc: initial SOC for electrified vehicles.  
+    /// aux_in_kw: aux_in_kw override.  Array of same length as cyc.time_s.  
+    ///     Default of None causes veh.aux_kw to be used.
+    pub fn sim_drive_py(
+        &mut self,
+        init_soc: Option<f64>,
+        aux_in_kw_override: Option<Vec<f64>>,
+    ) -> PyResult<()> {
+        let aux_in_kw_override = aux_in_kw_override.map(Array1::from);
+        self.sim_drive(init_soc, aux_in_kw_override);
+        Ok(())
+    }
+
     /// Receives second-by-second cycle information, vehicle properties,
     /// and an initial state of charge and runs sim_drive_step to perform a
     /// backward facing powertrain simulation. Method 'sim_drive' runs this
@@ -246,6 +275,149 @@ impl VehicleThermal {
         self.walk(init_soc, aux_in_kw_override);
         Ok(())
     }
+
+    #[pyo3(name = "init_for_step")]
+    /// This is a specialty method which should be called prior to using
+    /// sim_drive_step in a loop.
+    /// Arguments
+    /// ------------
+    /// init_soc: initial battery state-of-charge (SOC) for electrified vehicles
+    /// aux_in_kw: aux_in_kw override.  Array of same length as cyc.time_s.  
+    ///         Default of None causes veh.aux_kw to be used.
+    pub fn init_for_step_py(
+        &mut self,
+        init_soc:f64,
+        aux_in_kw_override: Option<Vec<f64>>
+    ) -> PyResult<()> {
+        let aux_in_kw_override = aux_in_kw_override.map(Array1::from);
+        self.init_for_step(init_soc, aux_in_kw_override);
+        Ok(())
+    }
+
+    /// Step through 1 time step.
+    pub fn sim_drive_step(&mut self) -> PyResult<()> {
+        self.step();
+        Ok(())
+    }
+    
+    #[pyo3(name = "solve_step")]
+    /// Perform all the calculations to solve 1 time step.
+    pub fn solve_step_py(&mut self, i: usize) -> PyResult<()> {
+        self.solve_step(i);
+        Ok(())
+    }
+
+    #[pyo3(name = "set_misc_calcs")]
+    /// Sets misc. calculations at time step 'i'
+    /// Arguments:
+    /// ----------
+    /// i: index of time step
+    pub fn set_misc_calcs_py(&mut self, i: usize) -> PyResult<()> {
+        self.set_misc_calcs(i);
+        Ok(())
+    }
+
+    #[pyo3(name = "set_comp_lims")]
+    // Calculate actual speed achieved if vehicle hardware cannot achieve trace speed.
+    // Arguments
+    // ------------
+    // i: index of time step
+    pub fn set_comp_lims_py(&mut self, i: usize) -> PyResult<()> {
+        self.set_comp_lims(i);
+        Ok(())
+    }
+
+    #[pyo3(name = "set_power_calcs")]
+    /// Calculate power requirements to meet cycle and determine if
+    /// cycle can be met.
+    /// Arguments
+    /// ------------
+    /// i: index of time step
+    pub fn set_power_calcs_py(&mut self, i: usize) -> PyResult<()> {
+        self.set_power_calcs(i);
+        Ok(())
+    }
+
+    #[pyo3(name = "set_ach_speed")]
+    // Calculate actual speed achieved if vehicle hardware cannot achieve trace speed.
+    // Arguments
+    // ------------
+    // i: index of time step
+    pub fn set_ach_speed_py(&mut self, i: usize) -> PyResult<()> {
+        self.set_ach_speed(i);
+        Ok(())
+    }
+
+    #[pyo3(name = "set_hybrid_cont_calcs")]
+    /// Hybrid control calculations.
+    /// Arguments
+    /// ------------
+    /// i: index of time step
+    pub fn set_hybrid_cont_calcs_py(&mut self, i: usize) -> PyResult<()> {
+        self.set_hybrid_cont_calcs(i);
+        Ok(())
+    }
+
+    #[pyo3(name = "set_fc_forced_state")]
+    /// Calculate control variables related to engine on/off state
+    /// Arguments
+    /// ------------
+    /// i: index of time step
+    /// `_py` extension is needed to avoid name collision with getter/setter methods
+    pub fn set_fc_forced_state_py(&mut self, i: usize) -> PyResult<()> {
+        self.set_fc_forced_state_rust(i);
+        Ok(())
+    }
+
+    #[pyo3(name = "set_hybrid_cont_decisions")]
+    /// Hybrid control decisions.
+    /// Arguments
+    /// ------------
+    /// i: index of time step
+    pub fn set_hybrid_cont_decisions_py(&mut self, i: usize) -> PyResult<()> {
+        self.set_hybrid_cont_decisions(i);
+        Ok(())
+    }
+
+    #[pyo3(name = "set_fc_power")]
+    /// Sets power consumption values for the current time step.
+    /// Arguments
+    /// ------------
+    /// i: index of time step
+    pub fn set_fc_power_py(&mut self, i: usize) -> PyResult<()> {
+        self.set_fc_power(i);
+        Ok(())
+    }
+
+    #[pyo3(name = "set_time_dilation")]
+    /// Sets the time dilation for the current step.
+    /// Arguments
+    /// ------------
+    /// i: index of time step
+    pub fn set_time_dilation_py(&mut self, i: usize) -> PyResult<()> {
+        self.set_time_dilation(i);
+        Ok(())
+    }
+
+    #[pyo3(name = "set_post_scalars")]
+    /// Sets scalar variables that can be calculated after a cycle is run.
+    /// This includes mpgge, various energy metrics, and others
+    pub fn set_post_scalars_py(&mut self) -> PyResult<()> {
+        self.set_post_scalars();
+        Ok(())
+    }
+
+    /// Return length of time arrays
+    pub fn len(&self) -> usize {
+        self.sd.cyc.time_s.len()
+    }    
+
+    /// added to make clippy happy
+    /// not sure whether there is any benefit to this or not for our purposes
+    /// Return self.cyc.time_is.is_empty()
+    pub fn is_empty(&self) -> bool {
+        self.sd.cyc.time_s.is_empty()
+    }
 )]
 #[pyclass]
 #[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
@@ -264,7 +436,74 @@ impl SimDriveHot {
         Self::from_file_parser(filename).unwrap()
     }
 
+    pub fn gap_to_lead_vehicle_m(&self) -> Array1<f64> {
+        self.sd.gap_to_lead_vehicle_m()
+    }
+
+    pub fn sim_drive(&mut self, init_soc: Option<f64>, aux_in_kw_override: Option<Array1<f64>>) {
+        self.sd.sim_drive(init_soc, aux_in_kw_override).unwrap();
+    }
+
     pub fn walk(&mut self, init_soc: f64, aux_in_kw_override: Option<Array1<f64>>) {
         self.sd.walk(init_soc, aux_in_kw_override).unwrap();
+    }
+
+    pub fn init_for_step(&mut self, init_soc: f64, aux_in_kw_override: Option<Array1<f64>>) {
+        self.sd.init_for_step(init_soc, aux_in_kw_override).unwrap();
+    }
+
+    pub fn set_speed_for_target_gap_using_idm(&mut self, i: usize) {
+        self.sd.set_speed_for_target_gap_using_idm(i);
+    }
+
+    pub fn set_speed_for_target_gap(&mut self, i: usize) {
+        self.sd.set_speed_for_target_gap(i);
+    }
+
+    pub fn step(&mut self) {
+        self.sd.step().unwrap();
+    }
+
+    pub fn solve_step(&mut self, i: usize) {
+        self.sd.solve_step(i).unwrap();
+    }
+
+    pub fn set_misc_calcs(&mut self, i: usize) {
+        self.sd.set_misc_calcs(i).unwrap();
+    }
+
+    pub fn set_comp_lims(&mut self, i: usize) {
+        self.sd.set_comp_lims(i).unwrap();
+    }
+
+    pub fn set_power_calcs(&mut self, i: usize) {
+        self.sd.set_power_calcs(i).unwrap();
+    }
+
+    pub fn set_ach_speed(&mut self, i: usize) {
+        self.sd.set_ach_speed(i).unwrap();
+    }
+
+    pub fn set_hybrid_cont_calcs(&mut self, i: usize) {
+        self.sd.set_hybrid_cont_calcs(i).unwrap();
+    }
+
+    pub fn set_fc_forced_state_rust(&mut self, i: usize) {
+        self.sd.set_fc_forced_state_rust(i).unwrap();
+    }
+
+    pub fn set_hybrid_cont_decisions(&mut self, i: usize) {
+        self.sd.set_hybrid_cont_decisions(i).unwrap();
+    }
+
+    pub fn set_fc_power(&mut self, i: usize) {
+        self.sd.set_fc_power(i).unwrap();
+    }
+    pub fn set_time_dilation(&mut self, i: usize) {
+        self.sd.set_time_dilation(i).unwrap();
+    }
+
+    pub fn set_post_scalars(&mut self) {
+        self.sd.set_post_scalars().unwrap();
     }
 }
