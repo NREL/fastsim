Fix issue with elevation calculation

fastsim/tests/test_following.py
@@ -985,9 +985,9 @@ class TestFollowing(unittest.TestCase):
             cyc = fastsim.cycle.make_cycle(
                 [0.0 , 10.0 , 20.0 , 30.0 , 40.0 , 50.0 , 99.0],
                 [0.0 , 10.0 , 10.0 , 10.0 , 10.0 ,  0.0 ,  0.0],
-                [0.01,  0.01,  0.0 , -0.01, -0.01,  0.0 ,  0.0],
+                [0.00,  0.00,  0.01, 0.00, -0.01 ,  0.0 ,  0.0],
             )
-            cyc = fastsim.cycle.resample(cyc, new_dt=0.1, hold_keys={'grade'})
+            cyc = fastsim.cycle.resample(cyc, new_dt=0.1, hold_keys_next={'grade'})
             cyc = fastsim.cycle.Cycle.from_dict(cyc)
             veh = fastsim.vehicle.Vehicle.from_vehdb(5)
             sd = fastsim.simdrive.SimDrive(cyc, veh)
@@ -998,10 +998,12 @@ class TestFollowing(unittest.TestCase):
             sd.sim_drive()
             ts0 = sd.cyc0.time_s
             dds0 = sd.cyc0.dist_m
-            hs0 = sd.cyc0.delta_elev_m
+            # TODO: should use sd.cyc0.delta_elev_m but that currently is not as accurate
+            hs0 = np.cumsum(np.cos(np.arctan(sd.cyc0.grade)) * sd.cyc0.dist_v2_m * sd.cyc0.grade)
             ts1 = sd.cyc.time_s
             dds1 = sd.cyc.dist_m
-            hs1 = sd.cyc.delta_elev_m
+            # TODO: should use sd.cyc0.delta_elev_m but that currently is not as accurate
+            hs1 = np.cumsum(np.cos(np.arctan(sd.cyc.grade)) * sd.cyc.dist_v2_m * sd.cyc.grade)
             self.assertAlmostEqual(sum(dds0), sum(dds1), places=-1)
             self.assertAlmostEqual(hs0[-1], hs1[-1], places=6)
             def calc_dist_of_peak_elev(dds, hs):