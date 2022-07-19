9cf0992aeb3bab0c6ba34d4b95a12f04dcd4b8bf
Kyle Carow <kcarow@nrel.gov>
2022-07-19 14:10:57 -0400

    added files that were forgotten in last commit

--- fastsim/docs/fusion_thermal_cal.py
+++ fastsim/docs/fusion_thermal_cal.py
@@ -1,1 +1,35 @@
+# %%
 import fastsim as fsim
+
+# Prepare SimDrives
+trips = ["trip_udds", "trip_us06"]
+cycles = [
+    fsim.cycle.Cycle.from_file("udds").to_rust(),
+    fsim.cycle.Cycle.from_file("us06")
+]
+assert len(cycles) == len(trips)
+vehicles = [fsim.vehicle.Vehicle.from_file("2012_Ford_Fusion.csv").to_rust()] * len(trips)
+sim_drives = {trip: fsim.SimDriveHot(cyc, veh) for trip, cyc, veh in zip(trips, cycles, vehicles)}
+
+objectives = fsim.calibration.ModelErrors(
+    sim_drives=sim_drives,
+    objectives=[
+        ("net_kj",),
+    ],
+    params=[
+        ("veh", "drag_coef"),
+    ],
+    verbose=False
+)
+
+problem = fsim.calibration.CalibrationProblem(
+    err=objectives,
+    param_bounds=[
+        (1.5, 3),
+    ],
+)
+
+# %%
+problem.minimize()
+
+print(problem.res.X)
