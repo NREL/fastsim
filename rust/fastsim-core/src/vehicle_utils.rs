4a2493834ffbcf7856389600109238d08f462190
Puligundla <vpuligun@nrel.gov>
2022-11-07 10:06:24 -0700

    Get vehicle data from fueleconomy.gov

--- rust/fastsim-core/src/vehicle_utils.rs
+++ rust/fastsim-core/src/vehicle_utils.rs
@@ -2,8 +2,10 @@
 
 use argmin::core::{CostFunction, Error, Executor, OptimizationResult, State};
 use argmin::solver::neldermead::NelderMead;
+use curl::easy::Easy;
 use ndarray::{array, Array1};
 use polynomial::Polynomial;
+use serde_xml_rs::from_str;
 
 use crate::air::*;
 use crate::cycle::RustCycle;
@@ -41,19 +43,19 @@ pub struct VehicleDataFE {
     alt_veh_type: String,
     #[serde(rename = "city08")]
     /// City MPG for fuel 1
-    city_mpg_fuel1: f64,
+    city_mpg_fuel1: i32,
     #[serde(rename = "cityA08")]
     /// City MPG for fuel 2
-    city_mpg_fuel2: f64,
+    city_mpg_fuel2: i32,
     #[serde(rename = "co2")]
     /// Tailpipe CO2 emissions in grams/mile
-    co2_g_per_mi: f64,
+    co2_g_per_mi: i32,
     #[serde(rename = "comb08")]
     /// Combined MPG for fuel 1
-    comb_mpg_fuel1: f64,
+    comb_mpg_fuel1: i32,
     #[serde(rename = "combA08")]
     /// Combined MPG for fuel 2
-    comb_mpg_fuel2: f64,
+    comb_mpg_fuel2: i32,
     #[serde(default)]
     /// Number of engine cylinders
     cylinders: String,
@@ -73,7 +75,7 @@ pub struct VehicleDataFE {
     ev_motor_kw: String,
     #[serde(rename = "feScore")]
     /// EPA fuel economy score
-    fe_score: f64,
+    fe_score: i32,
     #[serde(rename = "fuelType")]
     /// Combined vehicle fuel type (fuel 1 and fuel 2)
     fuel_type: String,
@@ -85,13 +87,13 @@ pub struct VehicleDataFE {
     fuel2: String,
     #[serde(rename = "ghgScore")]
     /// EPA GHG Score
-    ghg_score: f64,
+    ghg_score: i32,
     #[serde(rename = "highway08")]
     /// Highway MPG for fuel 1
-    highway_mpg_fuel1: f64,
+    highway_mpg_fuel1: i32,
     #[serde(rename = "highwayA08")]
     /// Highway MPG for fuel 2
-    highway_mpg_fuel2: f64,
+    highway_mpg_fuel2: i32,
     /// Manufacturer
     make: String,
     #[serde(rename = "mfrCode")]
@@ -104,13 +106,13 @@ pub struct VehicleDataFE {
     phev_blended: bool,
     #[serde(rename = "phevCity")]
     /// EPA composite gasoline-electricity city MPGe
-    phev_city_mpge: f64,
+    phev_city_mpge: i32,
     #[serde(rename = "phevComb")]
     /// EPA composite gasoline-electricity combined MPGe
-    phev_comb_mpge: f64,
+    phev_comb_mpge: i32,
     #[serde(rename = "phevHwy")]
     /// EPA composite gasoline-electricity highway MPGe
-    phev_hwy_mpge: f64,
+    phev_hwy_mpge: i32,
     #[serde(rename = "startStop")]
     /// Stop-start technology
     start_stop: String,
@@ -146,11 +148,63 @@ struct EmissionsInfoFE {
     /// EPA smog rating
     score: f64,
     /// SmartWay score
-    smartway_score: f64,
+    smartway_score: i32,
     /// Vehicle emission standard code
     standard: String,
     /// Vehicle emission standard
     std_text: String,
+}
+
+pub fn get_fuel_economy_gov_data(year: &str, make: &str, model: &str) -> Result<VehicleDataFE, Error> {
+    // Gets data from fueleconomy.gov for the given vehicle
+    //
+    // Arguments:
+    // ----------
+    // year: Vehicle year
+    // make: Vehicle make
+    // model: Vehicle model
+    //
+    // Returns:
+    // --------
+    // vehicle_data_fe: Data for the given vehicle from fueleconomy.gov
+    let mut handle: Easy = Easy::new();
+    let url: String = format!("https://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year={year}&make={make}&model={model}").replace(' ', "%20");
+    handle.url(&url)?;
+    let mut buf: String = String::new();
+    {
+        let mut transfer = handle.transfer();
+        transfer.write_function(|data| {
+            buf.push_str(std::str::from_utf8(data).unwrap());
+            Ok(data.len())
+        })?;
+        transfer.perform()?;
+    }
+
+    let vehicle_options: VehicleOptionsFE = from_str(&buf)?;
+    let mut index: usize = 0;
+    if vehicle_options.options.len() > 1 {
+        println!("Multiple engine configurations found. Please enter the index of the correct one.");
+        for i in 0..vehicle_options.options.len() {
+            println!("{i}: {}", vehicle_options.options[i].transmission);
+        }
+        let mut input: String = String::new();
+        let _num_bytes:usize = std::io::stdin().read_line(&mut input)?;
+        index = input.trim().parse()?;
+    }
+
+    handle.url(&format!("https://www.fueleconomy.gov/ws/rest/vehicle/{}", vehicle_options.options[index].id))?;
+    let mut veh_buf: String = String::new();
+    {
+        let mut transfer = handle.transfer();
+        transfer.write_function(|data| {
+            veh_buf.push_str(std::str::from_utf8(data).unwrap());
+            Ok(data.len())
+        })?;
+        transfer.perform()?;
+    }
+
+    let vehicle_data_fe: VehicleDataFE = from_str(&veh_buf)?;
+    return Ok(vehicle_data_fe);
 }
 
 #[allow(non_snake_case)]
@@ -217,7 +271,7 @@ pub fn abc_to_drag_coeffs(
     if simdrive_optimize.unwrap_or(true) {
         let cost: GetError = GetError {
             cycle: &cyc,
-            vehicle: &veh,
+            vehicle: veh,
             dyno_func_lb: &dyno_func_lb,
         };
         let solver: NelderMead<Array1<f64>, f64> =
@@ -299,12 +353,11 @@ impl CostFunction for GetError<'_> {
             .indexed_iter()
             .filter_map(|(index, &item)| (item < 0.1).then(|| index))
             .collect();
-        let cutoff: usize;
-        if cutoff_vec.len() == 0 {
-            cutoff = sd_coast.mps_ach.len();
+        let cutoff: usize = if cutoff_vec.is_empty() {
+            sd_coast.mps_ach.len()
         } else {
-            cutoff = cutoff_vec[0];
-        }
+            cutoff_vec[0]
+        };
 
         return Ok(get_error_val(
             (Array::from_vec(vec![1000.0; sd_coast.mps_ach.len()])
@@ -360,5 +413,65 @@ mod vehicle_utils_tests {
         assert!((0.0068603812443132645 - wheel_rr_coef).abs() < 1e-6);
         assert_eq!(drag_coef, veh.drag_coef);
         assert_eq!(wheel_rr_coef, veh.wheel_rr_coef);
+    }
+
+    #[test]
+    fn test_get_fuel_economy_gov_data() {
+        let year = "2022";
+        let make = "Toyota";
+        let model = "Prius Prime";
+        let prius_prime_fe_gov_data: VehicleDataFE = get_fuel_economy_gov_data(year, make, model).unwrap();
+        println!("FuelEconomy.gov: {} {} {}", prius_prime_fe_gov_data.year, prius_prime_fe_gov_data.make, prius_prime_fe_gov_data.model);
+        
+        let emissions_info1: EmissionsInfoFE = EmissionsInfoFE {
+            efid: String::from("NTYXV01.8P35"),
+            score: 7.0,
+            smartway_score: 1,
+            standard: String::from("L3SULEV30"),
+            std_text: String::from("California LEV-III SULEV30")
+        };
+        let emissions_info2: EmissionsInfoFE = EmissionsInfoFE {
+            efid: String::from("NTYXV01.8P35"),
+            score: 7.0,
+            smartway_score: 1,
+            standard: String::from("T3B30"),
+            std_text: String::from("Federal Tier 3 Bin 30")
+        };
+        let prius_prime_fe_truth: VehicleDataFE = VehicleDataFE {
+            alt_veh_type: String::from("Plug-in Hybrid"),
+            city_mpg_fuel1: 55,
+            city_mpg_fuel2: 145,
+            co2_g_per_mi: 78,
+            comb_mpg_fuel1: 54,
+            comb_mpg_fuel2: 133,
+            cylinders: String::from("4"),
+            displ: String::from("1.8"),
+            drive: String::from("Front-Wheel Drive"),
+            emissions_list: EmissionsListFE { emissions_info: vec![emissions_info1, emissions_info2] },
+            eng_dscr: String::from("PHEV"),
+            ev_motor_kw: String::from("22 and 53 kW AC Induction"),
+            fe_score: 10,
+            fuel_type: String::from("Regular Gas and Electricity"),
+            fuel1: String::from("Regular Gasoline"),
+            fuel2: String::from("Electricity"),
+            ghg_score: 10,
+            highway_mpg_fuel1: 53,
+            highway_mpg_fuel2: 121,
+            make: String::from("Toyota"),
+            mfr_code: String::from("TYX"),
+            model: String::from("Prius Prime"),
+            phev_blended: true,
+            phev_city_mpge: 83,
+            phev_comb_mpge: 78,
+            phev_hwy_mpge: 72,
+            start_stop: String::from("Y"),
+            trany: String::from("Automatic (variable gear ratios)"),
+            veh_class: String::from("Midsize Cars"),
+            year: 2022,
+            super_charge: String::new(),
+            turbo_charge: String::new(),
+        };
+        
+        assert_eq!(prius_prime_fe_gov_data, prius_prime_fe_truth);
     }
 }
