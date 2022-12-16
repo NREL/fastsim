f8f3f8ffff776308573322e4cc094ab58c0bb069
Kyle Carow <kcarow@nrel.gov>
2022-12-16 10:17:50 -0500

    set target for Rust logging

--- rust/fastsim-core/src/simdrive_impl.rs
+++ rust/fastsim-core/src/simdrive_impl.rs
@@ -2846,7 +2846,7 @@ impl RustSimDrive {
                 / (self.roadway_chg_kj + self.ess_dischg_kj + self.fuel_kj + self.ke_kj);
 
         if self.energy_audit_error.abs() > self.sim_params.energy_audit_error_tol {
-            log::warn!(
+            log::warn!(target: "fastsimrust",
                 "problem detected with conservation of energy; \
                     energy audit error: {:.5}",
                 self.energy_audit_error
@@ -2872,7 +2872,7 @@ impl RustSimDrive {
         if !self.sim_params.missed_trace_correction {
             if self.trace_miss_dist_frac > self.sim_params.trace_miss_dist_tol {
                 self.trace_miss = true;
-                log::warn!(
+                log::warn!(target: "fastsimrust",
                     "trace miss distance fraction {:.5} exceeds tolerance of {:.5}",
                     self.trace_miss_dist_frac,
                     self.sim_params.trace_miss_dist_tol
@@ -2880,7 +2880,7 @@ impl RustSimDrive {
             }
         } else if self.trace_miss_time_frac > self.sim_params.trace_miss_time_tol {
             self.trace_miss = true;
-            log::warn!(
+            log::warn!(target: "fastsimrust",
                 "trace miss time fraction {:.5} exceeds tolerance of {:.5}",
                 self.trace_miss_time_frac,
                 self.sim_params.trace_miss_time_tol
@@ -2891,7 +2891,7 @@ impl RustSimDrive {
             ndarrmax(&(self.mps_ach.clone() - self.cyc.mps.clone()).map(|x| x.abs()));
         if self.trace_miss_speed_mps > self.sim_params.trace_miss_speed_mps_tol {
             self.trace_miss = true;
-            log::warn!(
+            log::warn!(target: "fastsimrust",
                 "trace miss speed {:.5} m/s exceeds tolerance of {:.5} m/s",
                 self.trace_miss_speed_mps,
                 self.sim_params.trace_miss_speed_mps_tol
