84c8777705b4df878fb8f1fb174f320a68c8a643
Chad Baker <chad.baker@nrel.gov>
2023-03-24 17:14:31 -0400

    vehicle mass is zero somehow

--- rust/fastsim-cli/src/bin/fastsim-cli.rs
+++ rust/fastsim-cli/src/bin/fastsim-cli.rs
@@ -217,7 +217,7 @@ pub fn main() {
         let mut sim_drive = RustSimDrive::new(cyc, veh);
         // // this does nothing if it has already been called for the constructed `sim_drive`
         sim_drive.sim_drive(None, None).unwrap();
-        dbg!(sim_drive.veh.scenario_name);
+        dbg!(sim_drive.veh.veh_kg);
         println!("{}", sim_drive.mpgge);
     }
     // else {
