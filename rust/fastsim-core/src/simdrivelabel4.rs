4aa3d9eb311f7eafb0ef3abd19f47c18ae5f590d
Romia87 <126118320+Romia87@users.noreply.github.com>
2023-06-21 11:58:34 -0600

    changes to add_pyo3_api

--- rust/fastsim-core/src/simdrivelabel4.rs
+++ rust/fastsim-core/src/simdrivelabel4.rs
@@ -1,6 +1,4 @@
-// Modify the existing code from 1-125 to add pyo3 
-
-//! Module containing classes and methods for calculating label fuel economy.
+// Modify the existing code from 1-56 to add pyo3 
 
 use ndarray::Array;
 use std::collections::HashMap;
@@ -17,10 +15,8 @@ use crate::pyo3imports::*;
 use crate::simdrive::{RustSimDrive, RustSimDriveParams};
 use crate::vehicle;
 
-
-#[pyclass]
 #[derive(Default, Serialize, Deserialize, Clone, Debug, PartialEq, ApproxEq)]
-/// Label fuel economy values
+#[add_pyo3_api]
 pub struct LabelFe {
     pub veh: vehicle::RustVehicle,
     pub adj_params: AdjCoef,
@@ -39,9 +35,7 @@ pub struct LabelFe {
     pub adj_udds_ess_kwh_per_mi: f64,
     pub adj_hwy_ess_kwh_per_mi: f64,
     pub adj_comb_ess_kwh_per_mi: f64,
-    /// Range for combined city/highway
     pub net_range_miles: f64,
-    /// Utility factor
     pub uf: f64,
     pub net_accel: f64,
     pub res_found: String,
@@ -52,8 +46,270 @@ pub struct LabelFe {
     pub trace_miss_speed_mph: f64,
 }
 
-#[pyclass]
+impl LabelFe {
+    #[getter]
+    pub fn get_veh(&self) -> &vehicle::RustVehicle {
+        &self.veh
+    }
+
+    #[setter]
+    pub fn set_veh_py(&mut self, new_veh: vehicle::RustVehicle) {
+        self.veh = new_veh;
+    }
+
+    #[getter]
+    pub fn get_adj_params(&self) -> &AdjCoef {
+        &self.adj_params
+    }
+
+    #[setter]
+    pub fn set_adj_params_py(&mut self, new_params: AdjCoef) {
+        self.adj_params = new_params;
+    }
+
+    #[getter]
+    pub fn get_lab_udds_mpgge(&self) -> f64 {
+        self.lab_udds_mpgge
+    }
+
+    #[setter]
+    pub fn set_lab_udds_mpgge_py(&mut self, new_value: f64) {
+        self.lab_udds_mpgge = new_value;
+    }
+
+    #[getter]
+    pub fn get_lab_hwy_mpgge(&self) -> f64 {
+        self.lab_hwy_mpgge
+    }
+
+    #[setter]
+    pub fn set_lab_hwy_mpgge_py(&mut self, new_value: f64) {
+        self.lab_hwy_mpgge = new_value;
+    }
+
+    #[getter]
+    pub fn get_lab_comb_mpgge(&self) -> f64 {
+        self.lab_comb_mpgge
+    }
+
+    #[setter]
+    pub fn set_lab_comb_mpgge_py(&mut self, new_value: f64) {
+        self.lab_comb_mpgge = new_value;
+    }
+
+    #[getter]
+    pub fn get_lab_udds_kwh_per_mi(&self) -> f64 {
+        self.lab_udds_kwh_per_mi
+    }
+
+    #[setter]
+    pub fn set_lab_udds_kwh_per_mi_py(&mut self, new_value: f64) {
+        self.lab_udds_kwh_per_mi = new_value;
+    }
+
+    #[getter]
+    pub fn get_lab_hwy_kwh_per_mi(&self) -> f64 {
+        self.lab_hwy_kwh_per_mi
+    }
+
+    #[setter]
+    pub fn set_lab_hwy_kwh_per_mi_py(&mut self, new_value: f64) {
+        self.lab_hwy_kwh_per_mi = new_value;
+    }
+
+    #[getter]
+    pub fn get_lab_comb_kwh_per_mi(&self) -> f64 {
+        self.lab_comb_kwh_per_mi
+    }
+
+    #[setter]
+    pub fn set_lab_comb_kwh_per_mi_py(&mut self, new_value: f64) {
+        self.lab_comb_kwh_per_mi = new_value;
+    }
+
+    #[getter]
+    pub fn get_adj_udds_mpgge(&self) -> f64 {
+        self.adj_udds_mpgge
+    }
+
+    #[setter]
+    pub fn set_adj_udds_mpgge_py(&mut self, new_value: f64) {
+        self.adj_udds_mpgge = new_value;
+    }
+
+    #[getter]
+    pub fn get_adj_hwy_mpgge(&self) -> f64 {
+        self.adj_hwy_mpgge
+    }
+
+    #[setter]
+    pub fn set_adj_hwy_mpgge_py(&mut self, new_value: f64) {
+        self.adj_hwy_mpgge = new_value;
+    }
+
+    #[getter]
+    pub fn get_adj_comb_mpgge(&self) -> f64 {
+        self.adj_comb_mpgge
+    }
+
+    #[setter]
+    pub fn set_adj_comb_mpgge_py(&mut self, new_value: f64) {
+        self.adj_comb_mpgge = new_value;
+    }
+
+    #[getter]
+    pub fn get_adj_udds_kwh_per_mi(&self) -> f64 {
+        self.adj_udds_kwh_per_mi
+    }
+
+    #[setter]
+    pub fn set_adj_udds_kwh_per_mi_py(&mut self, new_value: f64) {
+        self.adj_udds_kwh_per_mi = new_value;
+    }
+
+    #[getter]
+    pub fn get_adj_hwy_kwh_per_mi(&self) -> f64 {
+        self.adj_hwy_kwh_per_mi
+    }
+
+    #[setter]
+    pub fn set_adj_hwy_kwh_per_mi_py(&mut self, new_value: f64) {
+        self.adj_hwy_kwh_per_mi = new_value;
+    }
+
+    #[getter]
+    pub fn get_adj_comb_kwh_per_mi(&self) -> f64 {
+        self.adj_comb_kwh_per_mi
+    }
+
+    #[setter]
+    pub fn set_adj_comb_kwh_per_mi_py(&mut self, new_value: f64) {
+        self.adj_comb_kwh_per_mi = new_value;
+    }
+
+    #[getter]
+    pub fn get_adj_udds_ess_kwh_per_mi(&self) -> f64 {
+        self.adj_udds_ess_kwh_per_mi
+    }
+
+    #[setter]
+    pub fn set_adj_udds_ess_kwh_per_mi_py(&mut self, new_value: f64) {
+        self.adj_udds_ess_kwh_per_mi = new_value;
+    }
+
+    #[getter]
+    pub fn get_adj_hwy_ess_kwh_per_mi(&self) -> f64 {
+        self.adj_hwy_ess_kwh_per_mi
+    }
+
+    #[setter]
+    pub fn set_adj_hwy_ess_kwh_per_mi_py(&mut self, new_value: f64) {
+        self.adj_hwy_ess_kwh_per_mi = new_value;
+    }
+
+    #[getter]
+    pub fn get_adj_comb_ess_kwh_per_mi(&self) -> f64 {
+        self.adj_comb_ess_kwh_per_mi
+    }
+
+    #[setter]
+    pub fn set_adj_comb_ess_kwh_per_mi_py(&mut self, new_value: f64) {
+        self.adj_comb_ess_kwh_per_mi = new_value;
+    }
+
+    #[getter]
+    pub fn get_net_range_miles(&self) -> f64 {
+        self.net_range_miles
+    }
+
+    #[setter]
+    pub fn set_net_range_miles_py(&mut self, new_value: f64) {
+        self.net_range_miles = new_value;
+    }
+
+    #[getter]
+    pub fn get_uf(&self) -> f64 {
+        self.uf
+    }
+
+    #[setter]
+    pub fn set_uf_py(&mut self, new_value: f64) {
+        self.uf = new_value;
+    }
+
+    #[getter]
+    pub fn get_net_accel(&self) -> f64 {
+        self.net_accel
+    }
+
+    #[setter]
+    pub fn set_net_accel_py(&mut self, new_value: f64) {
+        self.net_accel = new_value;
+    }
+
+    #[getter]
+    pub fn get_res_found(&self) -> &str {
+        &self.res_found
+    }
+
+    #[setter]
+    pub fn set_res_found_py(&mut self, new_value: String) {
+        self.res_found = new_value;
+    }
+
+    #[getter]
+    pub fn get_phev_calcs(&self) -> Option<&LabelFePHEV> {
+        self.phev_calcs.as_ref()
+    }
+
+    #[setter]
+    pub fn set_phev_calcs_py(&mut self, new_value: Option<LabelFePHEV>) {
+        self.phev_calcs = new_value;
+    }
+
+    #[getter]
+    pub fn get_adj_cs_comb_mpgge(&self) -> Option<f64> {
+        self.adj_cs_comb_mpgge
+    }
+
+    #[setter]
+    pub fn set_adj_cs_comb_mpgge_py(&mut self, new_value: Option<f64>) {
+        self.adj_cs_comb_mpgge = new_value;
+    }
+
+    #[getter]
+    pub fn get_adj_cd_comb_mpgge(&self) -> Option<f64> {
+        self.adj_cd_comb_mpgge
+    }
+
+    #[setter]
+    pub fn set_adj_cd_comb_mpgge_py(&mut self, new_value: Option<f64>) {
+        self.adj_cd_comb_mpgge = new_value;
+    }
+
+    #[getter]
+    pub fn get_net_phev_cd_miles(&self) -> Option<f64> {
+        self.net_phev_cd_miles
+    }
+
+    #[setter]
+    pub fn set_net_phev_cd_miles_py(&mut self, new_value: Option<f64>) {
+        self.net_phev_cd_miles = new_value;
+    }
+
+    #[getter]
+    pub fn get_trace_miss_speed_mph(&self) -> f64 {
+        self.trace_miss_speed_mph
+    }
+
+    #[setter]
+    pub fn set_trace_miss_speed_mph_py(&mut self, new_value: f64) {
+        self.trace_miss_speed_mph = new_value;
+    }
+}
+
 #[derive(Default, Serialize, Deserialize, Clone, Debug, PartialEq, ApproxEq)]
+#[add_pyo3_api]
 /// Label fuel economy values for a PHEV vehicle
 pub struct LabelFePHEV {
     pub regen_soc_buffer: f64,
@@ -61,88 +317,36 @@ pub struct LabelFePHEV {
     pub hwy: PHEVCycleCalc,
 }
 
-#[pyclass]
-#[derive(Default, Serialize, Deserialize, Clone, Debug, PartialEq, ApproxEq)]
-/// Label fuel economy calculations for a specific cycle of a PHEV vehicle
-pub struct PHEVCycleCalc {
-    /// Charge depletion battery kW-hr
-    pub cd_ess_kwh: f64,
-    pub cd_ess_kwh_per_mi: f64,
-    /// Charge depletion fuel gallons
-    pub cd_fs_gal: f64,
-    pub cd_fs_kwh: f64,
-    pub cd_mpg: f64,
-    /// Number of cycles in charge depletion mode, up to transition
-    pub cd_cycs: f64,
-    pub cd_miles: f64,
-    pub cd_lab_mpg: f64,
-    pub cd_adj_mpg: f64,
-    /// Fraction of transition cycles spent in charge depletion
-    pub cd_frac_in_trans: f64,
-    /// SOC change during 1 cycle
-    pub trans_init_soc: f64,
-    /// charge depletion battery kW-hr
-    pub trans_ess_kwh: f64,
-    pub trans_ess_kwh_per_mi: f64,
-    pub trans_fs_gal: f64,
-    pub trans_fs_kwh: f64,
-    /// charge sustaining battery kW-hr
-    pub cs_ess_kwh: f64,
-    pub cs_ess_kwh_per_mi: f64,
-    /// charge sustaining fuel gallons
-    pub cs_fs_gal: f64,
-    pub cs_fs_kwh: f64,
-    pub cs_mpg: f64,
-    pub lab_mpgge: f64,
-    pub lab_kwh_per_mi: f64,
-    pub lab_uf: f64,
-    pub lab_uf_gpm: Array1<f64>,
-    pub lab_iter_uf: Array1<f64>,
-    pub lab_iter_uf_kwh_per_mi: Array1<f64>,
-    pub lab_iter_kwh_per_mi: Array1<f64>,
-    pub adj_iter_mpgge: Array1<f64>,
-    pub adj_iter_kwh_per_mi: Array1<f64>,
-    pub adj_iter_cd_miles: Array1<f64>,
-    pub adj_iter_uf: Array1<f64>,
-    pub adj_iter_uf_gpm: Vec<f64>,
-    pub adj_iter_uf_kwh_per_mi: Array1<f64>,
-    pub adj_cd_miles: f64,
-    pub adj_cd_mpgge: f64,
-    pub adj_cs_mpgge: f64,
-    pub adj_uf: f64,
-    pub adj_mpgge: f64,
-    pub adj_kwh_per_mi: f64,
-    pub adj_ess_kwh_per_mi: f64,
-    pub delta_soc: f64,
-    /// Total number of miles in charge depletion mode, assuming constant kWh_per_mi
-    pub total_cd_miles: f64,
-}
-
-
-#[cfg(feature = "pyo3")]
-#[pymodule]
-fn label_fuel_economy(_py: Python, m: &PyModule) -> PyResult<()> {
-    // Register the make_accel_trace_py function
-    #[pyfn(m, "make_accel_trace")]
-    fn make_accel_trace_py() -> PyResult<RustCycle> {
-        let accel_cyc_secs = Array::range(0., 300., 0.1);
-        let mut accel_cyc_mps = Array::ones(accel_cyc_secs.len()) * 90.0 / MPH_PER_MPS;
-        accel_cyc_mps[0] = 0.0;
-
-        let rust_cycle = RustCycle::new(
-            accel_cyc_secs.to_vec(),
-            accel_cyc_mps.to_vec(),
-            Array::zeros(accel_cyc_secs.len()).to_vec(),
-            Array::zeros(accel_cyc_secs.len()).to_vec(),
-            String::from("accel"),
-        );
-
-        Ok(rust_cycle)
+#[pymethods]
+impl LabelFePHEV {
+    #[getter]
+    pub fn get_regen_soc_buffer(&self) -> f64 {
+        self.regen_soc_buffer() 
     }
 
-    // Add the make_accel_trace_py function to the module
-    m.add_function(wrap_pyfunction!(make_accel_trace_py, m)?)?;
+    #[setter]
+    pub fn set_regen_soc_buffer_py(&mut self, new_value: f64) {
+        self.regen_soc_buffer = new_value;
+    }
 
-    // Return Ok(()) to indicate successful execution
-    Ok(())
+    #[getter]
+    pub fn get_udds(&self) -> &PHEVCycleCalc {
+        &self.udds
+    }
+
+    #[setter]
+    pub fn set_udds_py(&mut self, new_value: PHEVCycleCalc) {
+        self.udds = new_value;
+    }
+
+    #[getter]
+    pub fn get_hwy(&self) -> &PHEVCycleCalc {
+        &self.hwy
+    }
+
+    #[setter]
+    pub fn set_hwy_py(&mut self, new_value: PHEVCycleCalc) {
+        self.hwy = new_value;
+    }
 }
+
