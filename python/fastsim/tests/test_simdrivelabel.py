8db67c928c81d0feb5bd0616f7fff279b6e1f0d9
Romia87 <126118320+Romia87@users.noreply.github.com>
2023-07-21 10:14:51 -0600

    run test__get_label_fe_conv

--- python/fastsim/tests/test_simdrivelabel.py
+++ python/fastsim/tests/test_simdrivelabel.py
@@ -12,10 +12,12 @@ class TestSimDriveLabel(unittest.TestCase):
         # see if you can figure out function body
         # ```
         # and adding the appropriate function wrapper in `fastsim-py/src/lib.rs`
-        label_fe = get_label_fe(veh, False, False)
+        label_fe, _ = fsr.get_label_fe(veh, False, False)  # Unpack the tuple
         # Because the full test is already implemented in Rust, we 
         # don't need a comprehensive check here.  
         self.assertEqual(label_fe.lab_udds_mpgge, 32.47503766676829)
+        self.assertEqual(label_fe.lab_hwy_mpgge, 42.265348793379445)
+        self.assertEqual(label_fe.lab_comb_mpgge, 36.25407690819302)
 
     def test_get_label_fe_phev(self):
         # TODO: fill this out similar to `test_get_label_fe_conv`
