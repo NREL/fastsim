0ab03518996fd22d877d13a18068681855616eab
Kyle Carow <Kyle.Carow@nrel.gov>
2023-08-21 11:37:37 -0600

    deleted extraneous building script

--- build_and_test_published.sh
+++
@@ -1,5 +0,0 @@
-# build and test with published version of `fastsim-proc-macros`
-(cd rust/fastsim-core/ && cargo test) && \
-(cd rust/fastsim-cli/ && cargo test) && \
-pip install -qe ".[dev]" && \
-pytest -v python/fastsim/tests/
