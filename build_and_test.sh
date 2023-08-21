c4b8f6074bc43b54fe9851052d42b72bb961b0ea
Chad Baker <chad.baker@nrel.gov>
2023-08-21 12:54:13 -0600

    fixed build_and_test.sh

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,5 +1,5 @@
 # build and test with local version of `fastsim-proc-macros`
-(cd rust/fastsim-core/ && cargo test --features dev-proc-macros) && \
+(cd rust/fastsim-core/ && cargo test) && \
 (cd rust/fastsim-cli/ && cargo test) && \
 pip install -qe ".[dev]" && \
 pytest -v python/fastsim/tests/
