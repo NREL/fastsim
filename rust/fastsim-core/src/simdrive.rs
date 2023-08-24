767efb0569e9721e8812cd9902dd7d3c023ef84f
Chad Baker <chad.baker@nrel.gov>
2023-08-24 11:53:59 -0600

    definitely fails in maturin step of build_and_test.sh

--- rust/fastsim-core/src/simdrive.rs
+++ rust/fastsim-core/src/simdrive.rs
@@ -136,228 +136,236 @@ impl Default for RustSimDriveParams {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-#[add_pyo3_api(
-    /// method for instantiating SimDriveRust
-    #[new]
-    pub fn __new__(cyc: RustCycle, veh: RustVehicle) -> Self {
-        Self::new(cyc, veh)
-    }
+macro_rules! sim_drive_pymethods {
+    () => {
+        /// method for instantiating SimDriveRust
+        #[new]
+        pub fn __new__(cyc: RustCycle, veh: RustVehicle) -> Self {
+            Self::new(cyc, veh)
+        }
 
-    pub fn __getnewargs__(&self) {
-        todo!();
-    }
+        pub fn __getnewargs__(&self) {
+            todo!();
+        }
 
-    // wrappers for core methods
+        // wrappers for core methods
 
-    #[pyo3(name = "gap_to_lead_vehicle_m")]
-    /// Provides the gap-with lead vehicle from start to finish
-    pub fn gap_to_lead_vehicle_m_py(&self) -> PyResult<Vec<f64>> {
-        Ok(self.gap_to_lead_vehicle_m().to_vec())
-    }
+        #[pyo3(name = "gap_to_lead_vehicle_m")]
+        /// Provides the gap-with lead vehicle from start to finish
+        pub fn gap_to_lead_vehicle_m_py(&self) -> PyResult<Vec<f64>> {
+            Ok(self.gap_to_lead_vehicle_m().to_vec())
+        }
 
-    #[pyo3(name = "sim_drive")]
-    /// Initialize and run sim_drive_walk as appropriate for vehicle attribute vehPtType.
-    /// Arguments
-    /// ------------
-    /// init_soc: initial SOC for electrified vehicles.
-    /// aux_in_kw: aux_in_kw override.  Array of same length as cyc.time_s.
-    ///     Default of None causes veh.aux_kw to be used.
-    pub fn sim_drive_py(
-        &mut self,
-        init_soc: Option<f64>,
-        aux_in_kw_override: Option<Vec<f64>>,
-    ) -> PyResult<()> {
-        let aux_in_kw_override = aux_in_kw_override.map(Array1::from);
-        Ok(self.sim_drive(init_soc, aux_in_kw_override)?)
-    }
+        #[pyo3(name = "sim_drive")]
+        /// Initialize and run sim_drive_walk as appropriate for vehicle attribute vehPtType.
+        /// Arguments
+        /// ------------
+        /// init_soc: initial SOC for electrified vehicles.
+        /// aux_in_kw: aux_in_kw override.  Array of same length as cyc.time_s.
+        ///     Default of None causes veh.aux_kw to be used.
+        pub fn sim_drive_py(
+            &mut self,
+            init_soc: Option<f64>,
+            aux_in_kw_override: Option<Vec<f64>>,
+        ) -> PyResult<()> {
+            let aux_in_kw_override = aux_in_kw_override.map(Array1::from);
+            Ok(self.sim_drive(init_soc, aux_in_kw_override)?)
+        }
 
-    /// Receives second-by-second cycle information, vehicle properties,
-    /// and an initial state of charge and runs sim_drive_step to perform a
-    /// backward facing powertrain simulation. Method 'sim_drive' runs this
-    /// iteratively to achieve correct SOC initial and final conditions, as
-    /// needed.
-    ///
-    /// Arguments
-    /// ------------
-    /// init_soc (optional): initial battery state-of-charge (SOC) for electrified vehicles
-    /// aux_in_kw: aux_in_kw override.  Array of same length as cyc.time_s.
-    ///         None causes veh.aux_kw to be used.
-    pub fn sim_drive_walk(
-        &mut self,
-        init_soc: f64,
-        aux_in_kw_override: Option<Vec<f64>>,
-    ) -> PyResult<()> {
-        let aux_in_kw_override = aux_in_kw_override.map(Array1::from);
-        Ok(self.walk(init_soc, aux_in_kw_override)?)
-    }
+        /// Receives second-by-second cycle information, vehicle properties,
+        /// and an initial state of charge and runs sim_drive_step to perform a
+        /// backward facing powertrain simulation. Method 'sim_drive' runs this
+        /// iteratively to achieve correct SOC initial and final conditions, as
+        /// needed.
+        ///
+        /// Arguments
+        /// ------------
+        /// init_soc (optional): initial battery state-of-charge (SOC) for electrified vehicles
+        /// aux_in_kw: aux_in_kw override.  Array of same length as cyc.time_s.
+        ///         None causes veh.aux_kw to be used.
+        pub fn sim_drive_walk(
+            &mut self,
+            init_soc: f64,
+            aux_in_kw_override: Option<Vec<f64>>,
+        ) -> PyResult<()> {
+            let aux_in_kw_override = aux_in_kw_override.map(Array1::from);
+            Ok(self.walk(init_soc, aux_in_kw_override)?)
+        }
 
-    /// Sets the intelligent driver model parameters for an eco-cruise driving trajectory.
-    /// This is a convenience method instead of setting the sim_params.idm* parameters yourself.
-    /// - by_microtrip: bool, if True, target speed is set by microtrip, else by cycle
-    /// - extend_fraction: float, the fraction of time to extend the cycle to allow for catch-up
-    ///     of the following vehicle
-    /// - blend_factor: float, a value between 0 and 1; only used of by_microtrip is True, blends
-    ///     between microtrip average speed and microtrip average speed when moving. Must be
-    ///     between 0 and 1 inclusive
-    pub fn activate_eco_cruise(
-        &mut self,
-        by_microtrip: Option<bool>,
-        extend_fraction: Option<f64>,
-        blend_factor: Option<f64>,
-        min_target_speed_m_per_s: Option<f64>,
-    ) -> PyResult<()> {
-        let by_microtrip: bool = by_microtrip.unwrap_or(false);
-        let extend_fraction: f64 = extend_fraction.unwrap_or(0.1);
-        let blend_factor: f64 = blend_factor.unwrap_or(0.0);
-        let min_target_speed_m_per_s = min_target_speed_m_per_s.unwrap_or(8.0);
+        /// Sets the intelligent driver model parameters for an eco-cruise driving trajectory.
+        /// This is a convenience method instead of setting the sim_params.idm* parameters yourself.
+        /// - by_microtrip: bool, if True, target speed is set by microtrip, else by cycle
+        /// - extend_fraction: float, the fraction of time to extend the cycle to allow for catch-up
+        ///     of the following vehicle
+        /// - blend_factor: float, a value between 0 and 1; only used of by_microtrip is True, blends
+        ///     between microtrip average speed and microtrip average speed when moving. Must be
+        ///     between 0 and 1 inclusive
+        pub fn activate_eco_cruise(
+            &mut self,
+            by_microtrip: Option<bool>,
+            extend_fraction: Option<f64>,
+            blend_factor: Option<f64>,
+            min_target_speed_m_per_s: Option<f64>,
+        ) -> PyResult<()> {
+            let by_microtrip: bool = by_microtrip.unwrap_or(false);
+            let extend_fraction: f64 = extend_fraction.unwrap_or(0.1);
+            let blend_factor: f64 = blend_factor.unwrap_or(0.0);
+            let min_target_speed_m_per_s = min_target_speed_m_per_s.unwrap_or(8.0);
             Ok(self.activate_eco_cruise_rust(
-                by_microtrip, extend_fraction, blend_factor, min_target_speed_m_per_s)?)
-    }
+                by_microtrip,
+                extend_fraction,
+                blend_factor,
+                min_target_speed_m_per_s,
+            )?)
+        }
 
-    #[pyo3(name = "init_for_step")]
-    /// This is a specialty method which should be called prior to using
-    /// sim_drive_step in a loop.
-    /// Arguments
-    /// ------------
-    /// init_soc: initial battery state-of-charge (SOC) for electrified vehicles
-    /// aux_in_kw: aux_in_kw override.  Array of same length as cyc.time_s.
-    ///         Default of None causes veh.aux_kw to be used.
-    pub fn init_for_step_py(
-        &mut self,
-        init_soc:f64,
-        aux_in_kw_override: Option<Vec<f64>>
-    ) -> PyResult<()> {
-        let aux_in_kw_override = aux_in_kw_override.map(Array1::from);
-        Ok(self.init_for_step(init_soc, aux_in_kw_override)?)
-    }
+        #[pyo3(name = "init_for_step")]
+        /// This is a specialty method which should be called prior to using
+        /// sim_drive_step in a loop.
+        /// Arguments
+        /// ------------
+        /// init_soc: initial battery state-of-charge (SOC) for electrified vehicles
+        /// aux_in_kw: aux_in_kw override.  Array of same length as cyc.time_s.
+        ///         Default of None causes veh.aux_kw to be used.
+        pub fn init_for_step_py(
+            &mut self,
+            init_soc: f64,
+            aux_in_kw_override: Option<Vec<f64>>,
+        ) -> PyResult<()> {
+            let aux_in_kw_override = aux_in_kw_override.map(Array1::from);
+            Ok(self.init_for_step(init_soc, aux_in_kw_override)?)
+        }
 
-    /// Step through 1 time step.
-    pub fn sim_drive_step(&mut self) -> PyResult<()> {
-        Ok(self.step()?)
-    }
+        /// Step through 1 time step.
+        pub fn sim_drive_step(&mut self) -> PyResult<()> {
+            Ok(self.step()?)
+        }
 
-    #[pyo3(name = "solve_step")]
-    /// Perform all the calculations to solve 1 time step.
-    pub fn solve_step_py(&mut self, i: usize) -> PyResult<()> {
-        Ok(self.solve_step(i)?)
-    }
+        #[pyo3(name = "solve_step")]
+        /// Perform all the calculations to solve 1 time step.
+        pub fn solve_step_py(&mut self, i: usize) -> PyResult<()> {
+            Ok(self.solve_step(i)?)
+        }
 
-    #[pyo3(name = "set_misc_calcs")]
-    /// Sets misc. calculations at time step 'i'
-    /// Arguments:
-    /// ----------
-    /// i: index of time step
-    pub fn set_misc_calcs_py(&mut self, i: usize) -> PyResult<()> {
-        Ok(self.set_misc_calcs(i)?)
-    }
+        #[pyo3(name = "set_misc_calcs")]
+        /// Sets misc. calculations at time step 'i'
+        /// Arguments:
+        /// ----------
+        /// i: index of time step
+        pub fn set_misc_calcs_py(&mut self, i: usize) -> PyResult<()> {
+            Ok(self.set_misc_calcs(i)?)
+        }
 
-    #[pyo3(name = "set_comp_lims")]
-    // Calculate actual speed achieved if vehicle hardware cannot achieve trace speed.
-    // Arguments
-    // ------------
-    // i: index of time step
-    pub fn set_comp_lims_py(&mut self, i: usize) -> PyResult<()> {
-        Ok(self.set_comp_lims(i)?)
-    }
+        #[pyo3(name = "set_comp_lims")]
+        // Calculate actual speed achieved if vehicle hardware cannot achieve trace speed.
+        // Arguments
+        // ------------
+        // i: index of time step
+        pub fn set_comp_lims_py(&mut self, i: usize) -> PyResult<()> {
+            Ok(self.set_comp_lims(i)?)
+        }
 
-    #[pyo3(name = "set_power_calcs")]
-    /// Calculate power requirements to meet cycle and determine if
-    /// cycle can be met.
-    /// Arguments
-    /// ------------
-    /// i: index of time step
-    pub fn set_power_calcs_py(&mut self, i: usize) -> PyResult<()> {
-        Ok(self.set_power_calcs(i)?)
-    }
+        #[pyo3(name = "set_power_calcs")]
+        /// Calculate power requirements to meet cycle and determine if
+        /// cycle can be met.
+        /// Arguments
+        /// ------------
+        /// i: index of time step
+        pub fn set_power_calcs_py(&mut self, i: usize) -> PyResult<()> {
+            Ok(self.set_power_calcs(i)?)
+        }
 
-    #[pyo3(name = "set_ach_speed")]
-    // Calculate actual speed achieved if vehicle hardware cannot achieve trace speed.
-    // Arguments
-    // ------------
-    // i: index of time step
-    pub fn set_ach_speed_py(&mut self, i: usize) -> PyResult<()> {
-        Ok(self.set_ach_speed(i)?)
-    }
+        #[pyo3(name = "set_ach_speed")]
+        // Calculate actual speed achieved if vehicle hardware cannot achieve trace speed.
+        // Arguments
+        // ------------
+        // i: index of time step
+        pub fn set_ach_speed_py(&mut self, i: usize) -> PyResult<()> {
+            Ok(self.set_ach_speed(i)?)
+        }
 
-    #[pyo3(name = "set_hybrid_cont_calcs")]
-    /// Hybrid control calculations.
-    /// Arguments
-    /// ------------
-    /// i: index of time step
-    pub fn set_hybrid_cont_calcs_py(&mut self, i: usize) -> PyResult<()> {
-        Ok(self.set_hybrid_cont_calcs(i)?)
-    }
+        #[pyo3(name = "set_hybrid_cont_calcs")]
+        /// Hybrid control calculations.
+        /// Arguments
+        /// ------------
+        /// i: index of time step
+        pub fn set_hybrid_cont_calcs_py(&mut self, i: usize) -> PyResult<()> {
+            Ok(self.set_hybrid_cont_calcs(i)?)
+        }
 
-    #[pyo3(name = "set_fc_forced_state")]
-    /// Calculate control variables related to engine on/off state
-    /// Arguments
-    /// ------------
-    /// i: index of time step
-    /// `_py` extension is needed to avoid name collision with getter/setter methods
-    pub fn set_fc_forced_state_py(&mut self, i: usize) -> PyResult<()> {
-        Ok(self.set_fc_forced_state_rust(i)?)
-    }
+        #[pyo3(name = "set_fc_forced_state")]
+        /// Calculate control variables related to engine on/off state
+        /// Arguments
+        /// ------------
+        /// i: index of time step
+        /// `_py` extension is needed to avoid name collision with getter/setter methods
+        pub fn set_fc_forced_state_py(&mut self, i: usize) -> PyResult<()> {
+            Ok(self.set_fc_forced_state_rust(i)?)
+        }
 
-    #[pyo3(name = "set_hybrid_cont_decisions")]
-    /// Hybrid control decisions.
-    /// Arguments
-    /// ------------
-    /// i: index of time step
-    pub fn set_hybrid_cont_decisions_py(&mut self, i: usize) -> PyResult<()> {
-        Ok(self.set_hybrid_cont_decisions(i)?)
-    }
+        #[pyo3(name = "set_hybrid_cont_decisions")]
+        /// Hybrid control decisions.
+        /// Arguments
+        /// ------------
+        /// i: index of time step
+        pub fn set_hybrid_cont_decisions_py(&mut self, i: usize) -> PyResult<()> {
+            Ok(self.set_hybrid_cont_decisions(i)?)
+        }
 
-    #[pyo3(name = "set_fc_power")]
-    /// Sets power consumption values for the current time step.
-    /// Arguments
-    /// ------------
-    /// i: index of time step
-    pub fn set_fc_power_py(&mut self, i: usize) -> PyResult<()> {
-        Ok(self.set_fc_power(i)?)
-    }
+        #[pyo3(name = "set_fc_power")]
+        /// Sets power consumption values for the current time step.
+        /// Arguments
+        /// ------------
+        /// i: index of time step
+        pub fn set_fc_power_py(&mut self, i: usize) -> PyResult<()> {
+            Ok(self.set_fc_power(i)?)
+        }
 
-    #[pyo3(name = "set_time_dilation")]
-    /// Sets the time dilation for the current step.
-    /// Arguments
-    /// ------------
-    /// i: index of time step
-    pub fn set_time_dilation_py(&mut self, i: usize) -> PyResult<()> {
-        Ok(self.set_time_dilation(i)?)
-    }
+        #[pyo3(name = "set_time_dilation")]
+        /// Sets the time dilation for the current step.
+        /// Arguments
+        /// ------------
+        /// i: index of time step
+        pub fn set_time_dilation_py(&mut self, i: usize) -> PyResult<()> {
+            Ok(self.set_time_dilation(i)?)
+        }
 
-    #[pyo3(name = "set_post_scalars")]
-    /// Sets scalar variables that can be calculated after a cycle is run.
-    /// This includes mpgge, various energy metrics, and others
-    pub fn set_post_scalars_py(&mut self) -> PyResult<()> {
-        Ok(self.set_post_scalars()?)
-    }
+        #[pyo3(name = "set_post_scalars")]
+        /// Sets scalar variables that can be calculated after a cycle is run.
+        /// This includes mpgge, various energy metrics, and others
+        pub fn set_post_scalars_py(&mut self) -> PyResult<()> {
+            Ok(self.set_post_scalars()?)
+        }
 
-    #[pyo3(name = "len")]
-    pub fn len_py(&self) -> usize {
-        self.len()
-    }
+        #[pyo3(name = "len")]
+        pub fn len_py(&self) -> usize {
+            self.len()
+        }
 
-    /// added to make clippy happy
-    /// not sure whether there is any benefit to this or not for our purposes
-    /// Return self.cyc.time_is.is_empty()
-    pub fn is_empty(&self) -> bool {
-        self.cyc.time_s.is_empty()
-    }
+        /// added to make clippy happy
+        /// not sure whether there is any benefit to this or not for our purposes
+        /// Return self.cyc.time_is.is_empty()
+        pub fn is_empty(&self) -> bool {
+            self.cyc.time_s.is_empty()
+        }
 
-    #[getter]
-    pub fn get_fs_cumu_mj_out_ach(&self) -> PyResult<Pyo3ArrayF64> {
-        Ok(
-            Pyo3ArrayF64::new(ndarrcumsum(&(self.fs_kw_out_ach.clone() * self.cyc.dt_s() * 1e-3)))
-        )
-    }
-    #[getter]
-    pub fn get_fc_cumu_mj_out_ach(&self) -> PyResult<Pyo3ArrayF64> {
-        Ok(
-            Pyo3ArrayF64::new(ndarrcumsum(&(self.fc_kw_out_ach.clone() * self.cyc.dt_s() * 1e-3)))
-        )
-    }
-)]
+        #[getter]
+        pub fn get_fs_cumu_mj_out_ach(&self) -> PyResult<Pyo3ArrayF64> {
+            Ok(Pyo3ArrayF64::new(ndarrcumsum(
+                &(self.fs_kw_out_ach.clone() * self.cyc.dt_s() * 1e-3),
+            )))
+        }
+        #[getter]
+        pub fn get_fc_cumu_mj_out_ach(&self) -> PyResult<Pyo3ArrayF64> {
+            Ok(Pyo3ArrayF64::new(ndarrcumsum(
+                &(self.fc_kw_out_ach.clone() * self.cyc.dt_s() * 1e-3),
+            )))
+        }
+    };
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[add_pyo3_api(sim_drive_pymethods!();)]
 pub struct RustSimDrive {
     pub hev_sim_count: usize,
     #[api(has_orphaned)]
