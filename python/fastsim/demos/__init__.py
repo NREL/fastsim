b2a5c048d94b74692a9f55e66180d1dc5ca9ff84
Chad Baker <calbaker@users.noreply.github.com>
2023-11-02 14:42:51 -0600

    changed print to warning
    Co-authored-by: Kyle Carow <40699307+kylecarow@users.noreply.github.com>

--- python/fastsim/demos/__init__.py
+++ python/fastsim/demos/__init__.py
@@ -1,4 +1,7 @@
-print(
-    "Module `fastsim.demos` has been imported.  This should only " 
-    + "happen during testing so you might be doing something wrong."
+import logging
+
+logger = logging.getLogger(__name__)
+logger.warning(
+    "Module `fastsim.demos` has been imported. This should only "
+    + "happen during testing, so you might be doing something wrong."
 )
