428abee71ef9bf1dccc2f0f53b644c5bbc8a1fa8
Kyle Carow <Kyle.Carow@nrel.gov>
2023-11-03 13:58:40 -0600

    shut annoying import warning up (it IS actually used)

--- rust/fastsim-core/src/imports.rs
+++ rust/fastsim-core/src/imports.rs
@@ -6,7 +6,9 @@ pub(crate) use serde::{Deserialize, Serialize};
 pub(crate) use std::cmp;
 pub(crate) use std::ffi::OsStr;
 pub(crate) use std::fs::File;
-pub(crate) use std::path::{Path, PathBuf};
+pub(crate) use std::path::Path;
+#[allow(unused_imports)]
+pub(crate) use std::path::PathBuf;
 
 pub(crate) use crate::traits::*;
 pub(crate) use crate::utils::*;
