41ec2a17de11813254cb9b3ff3751b04b5460e5d
Chad Baker <chad.baker@nrel.gov>
2024-01-17 13:40:04 -0700

    several failing python tests due to not having  feature enabled

--- rust/fastsim-py/src/lib.rs
+++ rust/fastsim-py/src/lib.rs
@@ -5,6 +5,7 @@ use pyo3imports::*;
 /// Function for adding Rust structs as Python Classes
 #[pymodule]
 fn fastsimrust(py: Python, m: &PyModule) -> PyResult<()> {
+    #[cfg(feature = "full")]
     pyo3_log::init();
     m.add_class::<cycle::RustCycle>()?;
     m.add_class::<vehicle::RustVehicle>()?;
@@ -28,20 +29,26 @@ fn fastsimrust(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<simdrive::simdrive_iter::SimDriveVec>()?;
 
     cycle::register(py, m)?;
+    #[cfg(feature = "full")]
     m.add_function(wrap_pyfunction!(vehicle_utils::abc_to_drag_coeffs, m)?)?;
     m.add_function(wrap_pyfunction!(make_accel_trace_py, m)?)?;
     m.add_function(wrap_pyfunction!(get_net_accel_py, m)?)?;
+    #[cfg(feature = "full")]
     m.add_function(wrap_pyfunction!(get_label_fe_py, m)?)?;
     m.add_function(wrap_pyfunction!(get_label_fe_phev_py, m)?)?;
+    #[cfg(feature = "full")]
     m.add_function(wrap_pyfunction!(get_label_fe_conv_py, m)?)?;
+    #[cfg(feature = "full")]
     m.add_function(wrap_pyfunction!(
         vehicle_utils::get_options_for_year_make_model,
         m
     )?)?;
+    #[cfg(feature = "full")]
     m.add_function(wrap_pyfunction!(
         vehicle_utils::vehicle_import_by_id_and_year,
         m
     )?)?;
+    #[cfg(feature = "full")]
     m.add_function(wrap_pyfunction!(vehicle_utils::import_all_vehicles, m)?)?;
     m.add_function(wrap_pyfunction!(vehicle_utils::export_vehicle_to_file, m)?)?;
 
