8dcfb44eb052e76dcdcefe644c59e9256c1891e5
Chad Baker <chad.baker@nrel.gov>
2024-01-29 15:35:59 -0700

    added `enabled_features` to pyi file

--- python/fastsim/fastsimrust.pyi
+++ python/fastsim/fastsimrust.pyi
@@ -2,9 +2,6 @@ from __future__ import annotations
 from typing_extensions import Self
 from typing import Dict, List, Tuple, Optional, ByteString
 from abc import ABC
-from fastsim.vehicle import VEHICLE_DIR
-import yaml
-from pathlib import Path
 
 class RustVec(ABC):
     def __repr__(self) -> str:
@@ -1074,4 +1071,5 @@ def get_label_fe_phev(
 def get_label_fe_conv(veh: RustVehicle) -> LabelFe:
    ...
    
+def enabled_features() -> List[str]: ...
    
