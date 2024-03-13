e39e5ca55705fb566ba467966c41a0e297a2f751
Kyle Carow <kylecarow@gmail.com>
2024-03-13 12:18:58 -0600

    progress on vehicle fields

--- rust/fastsim-core/src/uc.rs
+++ rust/fastsim-core/src/uc.rs
@@ -58,6 +58,8 @@ unit_const!(J, Energy, 1.0);
 unit_const!(MJ, Energy, 1.0E6);
 unit_const!(KJ, Energy, 1.0E3);
 
+unit_const!(KGM2, MomentOfInertia, 1.0);
+
 unit_const!(KGPM3, MassDensity, 1.0);
 
 unit_const!(MPS, Velocity, 1.0);
