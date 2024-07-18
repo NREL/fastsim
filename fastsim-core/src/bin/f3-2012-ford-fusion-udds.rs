176df6929e326bf21d3e99bf594cd87ad215520a
Chad Baker <chad.baker@nrel.gov>
2024-07-18 12:40:38 -0600

    doc string cleanup

--- fastsim-core/src/bin/f3-2012-ford-fusion-udds.rs
+++ fastsim-core/src/bin/f3-2012-ford-fusion-udds.rs
@@ -1,4 +1,7 @@
-/// binary for running the 2012 Ford Fusion in fastsim-3 with `cargo run --bin f3-2012-ford-fusion-udds`
+/// Binary for running the 2012 Ford Fusion in fastsim-3 with `cargo run --bin
+/// f3-2012-ford-fusion-udds`.  To run this for profiling, install [samply]
+/// (https://github.com/mstange/samply) and then run
+/// `samply record ./target/release/f3-2012-ford-fusion-udds`
 use fastsim_core::{prelude::*, traits::SerdeAPI};
 
 fn main() {
