461750f0ea495be1f92b9cf573f11e7a15ef390d
Robin Steuteville <robin.steuteville@nrel.gov>
2024-07-23 14:03:48 -0600

    updating comments and cleaning things up

--- fastsim-core/src/utils/interp/wrapper.rs
+++ fastsim-core/src/utils/interp/wrapper.rs
@@ -13,16 +13,6 @@
 use super::*;
 
 #[pyo3_api(
-//     #[getter]
-//     fn get_interpolator(&self) -> String {
-//         self.0.to_json().unwrap()
-//         // let py = pyo3::Python::new();
-//         // self.interpolator.into_py_dict_bound(py)
-//         // match &self(interpolator) {
-//         //     self(interpolator) => Some(interpolator.to_pydict()),
-//         //     _ => None,
-//         // }
-//     }
     /// Function to get x variable from enum variants
     #[getter("x")]
     pub fn x_py(&self) -> anyhow::Result<Vec<f64>> {
@@ -33,8 +23,8 @@ use super::*;
     /// # Arguments
     /// - `new_x`: updated `x` variable to replace the current `x` variable
     #[setter("__set_x")]
-    pub fn set_x_py(&mut self, new_x: Vec<f64>) -> anyhow::Result<()> {
-        self.0.set_x(new_x)
+    pub fn set_x_py(&mut self, new_x: Vec<f64>) -> PyResult<()> {
+        Ok(self.0.set_x(new_x)?)
     }
 
     /// Function to get f_x variable from enum variants
@@ -152,7 +142,6 @@ use super::*;
     /// Function to get values variable from enum variants
     #[getter("values")]
     pub fn values_py<'py>(&self, py: Python<'py>) -> anyhow::Result<&'py PyArrayDyn<f64>> {
-        // let py = pyo3::Python::<'unbound>::assume_gil_acquired;
         Ok(self.0.values()?.into_pyarray(py))
     }
 
@@ -165,7 +154,6 @@ use super::*;
         new_values: &PyArrayDyn<f64>,
     ) -> anyhow::Result<()> {
         self.0.set_values(new_values.to_owned_array())
-        // self.0.set_values(PyArray::from_array(py, new_values))
     }
 )]
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
@@ -184,102 +172,3 @@ impl InterpolatorWrapperVec {
 impl Init for InterpolatorWrapperVec {}
 
 impl SerdeAPI for InterpolatorWrapperVec {}
-
-// #[cfg(test)]
-// mod tests {
-//     use crate::vehicle::vehicle_model::Vehicle;
-
-//     use super::*;
-
-//     #[test]
-//     fn test_getters_and_setters() {
-//         // first, create ford vehicle from yaml
-//         let mut vehicle_2d = Vehicle::from_file(
-//             "/Users/rsteutev/Documents/GitHub/fastsim/tests/assets/2012_Ford_Fusion.yaml",
-//             false,
-//         )
-//         .unwrap();
-//         // then, update the interpolation -- create new yaml files for each interpolation type
-//         let mut fc_2d = vehicle_2d.pt_type.fc().unwrap().to_owned();
-//         let x = vec![0.05, 0.10, 0.15];
-//         let y = vec![0.10, 0.20, 0.30];
-//         let f_xy = vec![vec![0., 1., 2.], vec![3., 4., 5.], vec![6., 7., 8.]];
-//         fc_2d.eff_interp.0 = Interpolator::Interp2D(
-//             Interp2D::new(
-//                 x.clone(),
-//                 y.clone(),
-//                 f_xy.clone(),
-//                 Strategy::Linear,
-//                 Extrapolate::Error,
-//             )
-//             .unwrap(),
-//         );
-//         vehicle_2d.pt_type.set_fc(fc_2d).unwrap();
-//         vehicle_2d.to_file("/Users/rsteutev/Documents/GitHub/fastsim/tests/assets/2012_Ford_Fusion_2D_test.yaml").unwrap();
-
-//         // first, create ford vehicle from yaml
-//         let mut vehicle_3d = Vehicle::from_file(
-//             "/Users/rsteutev/Documents/GitHub/fastsim/tests/assets/2012_Ford_Fusion.yaml",
-//             false,
-//         )
-//         .unwrap();
-//         // then, update the interpolation -- create new yaml files for each interpolation type
-//         let mut fc_3D = vehicle_3d.pt_type.fc().unwrap().to_owned();
-//         let x = vec![0.05, 0.10, 0.15];
-//         let y = vec![0.10, 0.20, 0.30];
-//         let z = vec![0.20, 0.40, 0.60];
-//         let f_xyz = vec![
-//             vec![vec![0., 1., 2.], vec![3., 4., 5.], vec![6., 7., 8.]],
-//             vec![vec![9., 10., 11.], vec![12., 13., 14.], vec![15., 16., 17.]],
-//             vec![
-//                 vec![18., 19., 20.],
-//                 vec![21., 22., 23.],
-//                 vec![24., 25., 26.],
-//             ],
-//         ];
-//         fc_3D.eff_interp.0 = Interpolator::Interp3D(
-//             Interp3D::new(
-//                 x.clone(),
-//                 y.clone(),
-//                 z.clone(),
-//                 f_xyz.clone(),
-//                 Strategy::Linear,
-//                 Extrapolate::Error,
-//             )
-//             .unwrap(),
-//         );
-//         vehicle_3d.pt_type.set_fc(fc_3D).unwrap();
-//         vehicle_3d.to_file("/Users/rsteutev/Documents/GitHub/fastsim/tests/assets/2012_Ford_Fusion_3D_test.yaml").unwrap();
-
-//         // first, create ford vehicle from yaml
-//         let mut vehicle_nd = Vehicle::from_file(
-//             "/Users/rsteutev/Documents/GitHub/fastsim/tests/assets/2012_Ford_Fusion.yaml",
-//             false,
-//         )
-//         .unwrap();
-//         // then, update the interpolation -- create new yaml files for each interpolation type
-//         let mut fc_nd = vehicle_nd.pt_type.fc().unwrap().to_owned();
-//         let grid = vec![
-//             vec![0.05, 0.10, 0.15],
-//             vec![0.10, 0.20, 0.30],
-//             vec![0.20, 0.40, 0.60],
-//         ];
-//         let f_xyz = array![
-//             [[0., 1., 2.], [3., 4., 5.], [6., 7., 8.]],
-//             [[9., 10., 11.], [12., 13., 14.], [15., 16., 17.]],
-//             [[18., 19., 20.], [21., 22., 23.], [24., 25., 26.]],
-//         ]
-//         .into_dyn();
-//         fc_nd.eff_interp.0 = Interpolator::InterpND(
-//             InterpND::new(
-//                 grid.clone(),
-//                 f_xyz.clone(),
-//                 Strategy::Linear,
-//                 Extrapolate::Error,
-//             )
-//             .unwrap(),
-//         );
-//         vehicle_nd.pt_type.set_fc(fc_nd).unwrap();
-//         vehicle_nd.to_file("/Users/rsteutev/Documents/GitHub/fastsim/tests/assets/2012_Ford_Fusion_ND_test.yaml").unwrap();
-//     }
-// }
