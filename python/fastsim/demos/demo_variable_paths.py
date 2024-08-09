754085e9f4b9b1247ad177b5c43b27dd7ee3a202
Chad Baker <chad.baker@nrel.gov>
2024-08-09 13:14:53 -0600

    `to_dataframe` and other tests now handled in demo removed deprecated test assets added test asset for demo file created useful environment variables

--- python/fastsim/demos/demo_variable_paths.py
+++ python/fastsim/demos/demo_variable_paths.py
@@ -2,7 +2,9 @@
 Script demonstrating how to use variable_path_list() and history_path_list()
 demos to find the paths to variables within fastsim classes.
 """
+import os
 import fastsim as fsim
+import polars as pl
 
 # load 2012 Ford Fusion from file
 veh = fsim.Vehicle.from_file(
@@ -19,9 +21,25 @@ cyc = fsim.Cycle.from_resource("udds.csv")
 sd = fsim.SimDrive(veh, cyc)
 sd.walk()
 
+# whether to run assertions, enabled by default
+ENABLE_ASSERTS = os.environ.get("ENABLE_ASSERTS", "true").lower() == "true"
+# whether to override reference files used in assertions, disabled by default
+ENABLE_REF_OVERRIDE = os.environ.get("ENABLE_REF_OVERRIDE", "false").lower() == "true"
+# directory for reference files for checking sim results against expected results
+ref_dir = fsim.resources_root() / "demos/demo_variable_paths/"
+
+
 # print out all subpaths for variables in SimDrive
 print("List of variable paths for SimDrive:" + "\n".join(sd.variable_path_list()))
 print("\n")
+if ENABLE_ASSERTS:
+    with open(ref_dir / "variable_path_list_expected.txt", 'r') as f:
+        variable_path_list_expected = [line.strip() for line in f.readlines()]
+        assert variable_path_list_expected == sd.variable_path_list()
+if ENABLE_REF_OVERRIDE:
+    with open(ref_dir / "variable_path_list_expected.txt", 'w') as f:
+        for line in sd.variable_path_list():
+            f.write(line + "\n")
 
 # print out all subpaths for history variables in SimDrive
 print("List of history variable paths for SimDrive:" +  "\n".join(sd.history_path_list()))
@@ -29,3 +47,9 @@ print("\n")
 
 # print results as dataframe
 print("Results as dataframe:\n", sd.to_dataframe(), sep="")
+if ENABLE_ASSERTS:
+    to_dataframe_expected = pl.scan_csv(ref_dir / "to_dataframe_expected.csv")
+if ENABLE_REF_OVERRIDE:
+    df:pl.DataFrame = sd.to_dataframe().lazy().collect()
+    df.write_csv(ref_dir / "to_dataframe_expected.csv")
+
