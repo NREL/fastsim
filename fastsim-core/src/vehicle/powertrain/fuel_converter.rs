a9fda5db17339ab79291eade22c78d9ec505cb70
Kyle Carow <kylecarow@gmail.com>
2025-02-14 11:41:35 -0700

    pwr_type to pwr_out_type

--- fastsim-core/src/vehicle/powertrain/fuel_converter.rs
+++ fastsim-core/src/vehicle/powertrain/fuel_converter.rs
@@ -55,7 +55,7 @@ use std::f64::consts::PI;
 /// Struct for modeling [FuelConverter] (e.g. engine, fuel cell.) thermal plant
 #[non_exhaustive]
 pub struct FuelConverter {
-    pub pwr_type: FuelConverterPowerType,
+    pub pwr_out_type: FuelConverterPowerType,
     /// [Self] Thermal plant, including thermal management controls
     #[serde(default, skip_serializing_if = "FuelConverterThermalOption::is_none")]
     #[has_state]
--- fastsim-core/src/vehicle/vehicle_model/fastsim2_interface.rs
+++ fastsim-core/src/vehicle/vehicle_model/fastsim2_interface.rs
@@ -76,7 +76,7 @@ impl TryFrom<&fastsim_2::vehicle::RustVehicle> for PowertrainType {
                             pwr_for_peak_eff: uc::KW * f64::NAN, // this gets updated in `init`
                             // this means that aux power must include idle fuel
                             pwr_idle_fuel: si::Power::ZERO,
-                            pwr_type: FuelConverterPowerType::Mechanical,
+                            pwr_out_type: FuelConverterPowerType::Mechanical,
                             save_interval: Some(1),
                             history: Default::default(),
                         };
@@ -155,7 +155,7 @@ impl TryFrom<&fastsim_2::vehicle::RustVehicle> for PowertrainType {
                             pwr_for_peak_eff: uc::KW * f64::NAN, // this gets updated in `init`
                             // this means that aux power must include idle fuel
                             pwr_idle_fuel: si::Power::ZERO,
-                            pwr_type: if &f2veh.fc_eff_type == fastsim_2::vehicle::H2FC {
+                            pwr_out_type: if &f2veh.fc_eff_type == fastsim_2::vehicle::H2FC {
                                 FuelConverterPowerType::Electrical
                             } else {
                                 FuelConverterPowerType::Mechanical
