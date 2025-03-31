457e8ebaf5c9bb4038a43d0d38e22847bb4a022b
Chad Baker <chad.baker@nrel.gov>
2025-03-31 17:35:26 -0600

    down to only 1 failure!

---
+++ fastsim-core/src/vehicle/hvac/hvac_utils.rs
@@ -0,0 +1,19 @@
+use super::*;
+
+#[derive(Clone, Copy, Debug, Default, Deserialize, Serialize, PartialEq, IsVariant)]
+/// HVAC operating mode
+pub enum HvacMode {
+    /// Heating, i.e. greater than setpoint temperature plus deadband
+    Heating,
+    /// Cooling, i.e. less than setpoint temperature minus deadband
+    Cooling,
+    /// Inside deadband, i.e. greater than or equal to setpoint temperature
+    /// minus deadband and less than or equal to setpoint temperature plus
+    /// deadband
+    InsideDeadband,
+    /// Inactive
+    Inactive,
+    #[default]
+    /// Each time step sets to this value at the start to enforce updating
+    None,
+}
