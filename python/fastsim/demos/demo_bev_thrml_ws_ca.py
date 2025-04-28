5fcd32e195b85d12da9bd2534e8f5dfa1bbd465c
Chad Baker <chad.baker@nrel.gov>
2025-04-28 13:02:03 -0600

    fixed typo in method name

--- python/fastsim/demos/demo_bev_thrml_ws_ca.py
+++ python/fastsim/demos/demo_bev_thrml_ws_ca.py
@@ -29,7 +29,7 @@ temp_init_bat_and_cab = 22.0 + celsius_to_kelvin
 # %%
 
 # load 2020 Chevrolet Bolt BEV from file
-veh = fsim.Vehicle.from_resources("2020 Chevrolet Bolt EV.yaml")
+veh = fsim.Vehicle.from_resource("2020 Chevrolet Bolt EV.yaml")
 
 veh_dict = veh.to_pydict()
 veh_dict["cabin"]["LumpedCabin"]["state"]["temperature_kelvin"] = temp_init_bat_and_cab
