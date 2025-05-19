81646093b640cd4b320a5a8c22fdfe1aa2290fab
Robin Steuteville <robin.steuteville@nrel.gov>
2025-05-19 15:21:57 -0600

    updating fastsim2_interface.rs

--- fastsim-core/src/vehicle/vehicle_model/fastsim2_interface.rs
+++ fastsim-core/src/vehicle/vehicle_model/fastsim2_interface.rs
@@ -237,8 +237,8 @@ impl TryFrom<&fastsim_2::vehicle::RustVehicle> for PowertrainType {
                     em: ElectricMachine {
                         state: Default::default(),
                         eff_interp_achieved: InterpolatorEnum::new_1d(
-                            f2veh.mc_pwr_out_perc,
-                            f2veh.mc_eff_array,
+                            f2veh.mc_pwr_out_perc.clone(),
+                            f2veh.mc_eff_array.clone(),
                             strategy::LeftNearest,
                             Extrapolate::Error,
                         )?,
