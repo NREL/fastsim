b452c6804bf80d03d7ce6c8c327f99711621c89a
Chad Baker <chad.baker@nrel.gov>
2025-07-01 13:23:08 -0600

    fixed build error

--- fastsim-core/build.rs
+++ fastsim-core/build.rs
@@ -6,14 +6,14 @@ fn main() {
     assert!(bolt_source.exists());
     let bolt_target = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .join("resources/vehicles/2020 Chevrolet Bolt EV thrml.yaml");
+    std::fs::copy(bolt_source, &bolt_target).unwrap();
     assert!(bolt_target.exists());
-    std::fs::copy(bolt_source, bolt_target).unwrap();
 
     let sonata_source = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .join("../cal_and_val/thermal/f3-vehicles/2021_Hyundai_Sonata_Hybrid_Blue.yaml");
     assert!(sonata_source.exists());
     let sonata_target = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .join("resources/vehicles/2021_Hyundai_Sonata_Hybrid_Blue_thrml.yaml");
+    std::fs::copy(sonata_source, &sonata_target).unwrap();
     assert!(sonata_target.exists());
-    std::fs::copy(sonata_source, sonata_target).unwrap();
 }
