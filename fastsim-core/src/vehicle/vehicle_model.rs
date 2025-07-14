d28adc064ae48658f71fcec1de7b133a5e68c509
Chad Baker <chad.baker@nrel.gov>
2025-07-14 12:40:04 -0600

    added more asserts

--- fastsim-core/src/vehicle/vehicle_model.rs
+++ fastsim-core/src/vehicle/vehicle_model.rs
@@ -984,8 +984,10 @@ pub fn f3veh_with_f2_eff(f2veh: &fastsim_2::vehicle::RustVehicle, veh: &mut Vehi
         match &mut fc.eff_interp_from_pwr_out {
             InterpolatorEnum::Interp1D(interp1d) => {
                 assert_eq!(f2veh.fc_perc_out_array.len(), 100);
+                assert_eq!(interp1d.data.grid[0].len(), 12);
                 interp1d.data.grid = [f2veh.fc_perc_out_array.clone().into()];
                 assert_eq!(f2veh.fc_eff_array.len(), 100);
+                assert_eq!(interp1d.data.values.len(), 12);
                 interp1d.data.values = f2veh.fc_eff_array.clone().into();
                 interp1d.strategy = Strategy1DEnum::LeftNearest(strategy::LeftNearest);
             }
@@ -997,8 +999,10 @@ pub fn f3veh_with_f2_eff(f2veh: &fastsim_2::vehicle::RustVehicle, veh: &mut Vehi
         match &mut em.eff_interp_achieved {
             InterpolatorEnum::Interp1D(interp1d) => {
                 assert_eq!(f2veh.mc_perc_out_array.len(), 101);
+                assert_eq!(interp1d.data.grid[0].len(), 11);
                 interp1d.data.grid = [f2veh.fc_perc_out_array.clone().into()];
                 assert_eq!(f2veh.mc_full_eff_array.len(), 101);
+                assert_eq!(interp1d.data.values.len(), 11);
                 interp1d.data.values = f2veh.fc_eff_array.clone().into();
                 interp1d.strategy = Strategy1DEnum::LeftNearest(strategy::LeftNearest);
             }
