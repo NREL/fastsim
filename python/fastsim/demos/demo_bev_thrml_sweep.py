f1da8f9c6fb0977212110848788e1a46eed0af30
Chad Baker <chad.baker@nrel.gov>
2025-07-22 15:08:46 -0600

    produces useful plots

--- python/fastsim/demos/demo_bev_thrml_sweep.py
+++ python/fastsim/demos/demo_bev_thrml_sweep.py
@@ -2,6 +2,7 @@
 
 # %%
 import os
+from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -32,8 +33,17 @@ def try_walk(sd: fsim.SimDrive, loc: str) -> None:
         raise Exception(f"{loc}:\n{err}")
 
 
-te_amb_arr: list[float] = [t + celsius_to_kelvin for t in np.linspace(-7.0, 40.0, 20)]
-te_batt_and_cab_init_arr: list[float] = [t + celsius_to_kelvin for t in np.linspace(-7.0, 45.0, 20)]
+# array of ambient temperatures in kelvin
+te_amb_arr_k: list[float] = [t + celsius_to_kelvin for t in np.linspace(-7.0, 40.0, 20)]
+# array of init temperatures in kelvin
+te_batt_and_cab_init_arr_k: list[float] = [
+    t + celsius_to_kelvin for t in np.linspace(-7.0, 45.0, 20)
+]
+
+cyc_key = "cycle"
+te_amb_key = "te_amb [*C]"
+te_init_key = "te_init [*C]"
+ecr_key = "ECR [kW-hr/100mi]"
 
 
 def sweep():
@@ -42,50 +52,47 @@ def sweep():
     veh = fsim.Vehicle.from_resource("2020 Chevrolet Bolt EV thrml.yaml")
     veh.set_save_interval(1)
 
-    df_res = pd.DataFrame(
-        columns=["cycle", "te_amb [*C]", "te_init [*C]", "ECR [kW-hr/100mi]"],
-    )
+    res_list = []
+    for cyc_str in ["udds", "hwfet"]:
+        for te_amb_k in te_amb_arr_k:
+            for te_init_k in te_batt_and_cab_init_arr_k:
+                cyc = fsim.Cycle.from_resource(cyc_str + ".csv")
+                cyc_dict = cyc.to_pydict()
+                cyc_dict["temp_amb_air_kelvin"] = [te_amb_k] * cyc.len()
+                cyc = fsim.Cycle.from_pydict(cyc_dict)
 
-    for cyc_str, te_amb, te_init in zip(["udds", "hwfet"], te_amb_arr, te_batt_and_cab_init_arr):
-        cyc = fsim.Cycle.from_resource(cyc_str + ".csv")
-        cyc_dict = cyc.to_pydict()
-        cyc_dict["temp_amb_air_kelvin"] = [te_amb] * cyc.len()
-        cyc = fsim.Cycle.from_pydict(cyc_dict)
+                # setup initial conditions
+                veh_dict = veh.to_pydict()
+                veh_dict["pt_type"]["BEV"]["res"]["thrml"]["RESLumpedThermal"]["state"][
+                    "temperature_kelvin"
+                ] = te_init_k
+                veh_dict["pt_type"]["BEV"]["res"]["thrml"]["RESLumpedThermal"]["state"][
+                    "temp_prev_kelvin"
+                ] = te_init_k
+                veh_dict["cabin"]["LumpedCabin"]["state"]["temperature_kelvin"] = te_init_k
+                veh_dict["cabin"]["LumpedCabin"]["state"]["temp_prev_kelvin"] = te_init_k
 
-        # setup initial conditions
-        veh_dict = veh.to_pydict()
-        veh_dict["pt_type"]["BEV"]["res"]["thrml"]["RESLumpedThermal"]["state"][
-            "temperature_kelvin"
-        ] = te_init
-        veh_dict["pt_type"]["BEV"]["res"]["thrml"]["RESLumpedThermal"]["state"][
-            "temp_prev_kelvin"
-        ] = te_init
-        veh_dict["cabin"]["LumpedCabin"]["state"]["temperature_kelvin"] = te_init
-        veh_dict["cabin"]["LumpedCabin"]["state"]["temp_prev_kelvin"] = te_init
+                # simulate cycle
+                veh = fsim.Vehicle.from_pydict(veh_dict)
+                sd = fsim.SimDrive(veh, cyc, None)
+                try_walk(sd, f"`sd_prep`, te_amb: {te_amb_k}, te_init: {te_init_k}")
+                veh_dict_solved = sd.to_pydict()["veh"]
 
-        # simulate cycle
-        veh = fsim.Vehicle.from_pydict(veh_dict)
-        sd = fsim.SimDrive(veh, cyc, None)
-        try_walk(sd, f"`sd_prep`, te_amb: {te_amb}, te_init: {te_init}")
-        veh_dict_solved = sd.to_pydict()["veh"]
+                new_row = {
+                    cyc_key: cyc_str,
+                    te_amb_key: te_amb_k - celsius_to_kelvin,
+                    te_init_key: te_init_k - celsius_to_kelvin,
+                    ecr_key: veh_dict_solved["pt_type"]["BEV"]["res"]["state"][
+                        "energy_out_chemical_joules"
+                    ]
+                    / 1_000
+                    / 3_600
+                    / (veh_dict_solved["state"]["dist_meters"] / 1e3 / 1.61)
+                    * 100.0,
+                }
+                res_list.append(new_row)
 
-        new_row = pd.Series(
-            [
-                cyc_str,
-                te_amb - celsius_to_kelvin,
-                te_init - celsius_to_kelvin,
-                veh_dict_solved["pt_type"]["BEV"]["res"]["state"]["energy_out_chemical_joules"]
-                / 1_000
-                / 3_600
-                / (veh_dict_solved["state"]["dist_meters"] / 1e3 / 1.61),
-            ],
-        )
-        df_res = pd.concat(
-            [
-                df_res,
-                new_row,
-            ],
-        )
+    df_res = pd.DataFrame(res_list)
 
     return df_res
 
@@ -95,35 +102,114 @@ df_res = sweep()
 # if environment var `SHOW_PLOTS=false` is set, no plots are shown
 SHOW_PLOTS = os.environ.get("SHOW_PLOTS", "true").lower() == "true"
 
-te_amb_short = te_amb_arr[:: int(len(te_amb_arr) / 5)]
+# if environment var `SAVE_PLOTS=true` is set, plots are saved
+SAVE_PLOTS = os.environ.get("SAVE_PLOTS", "true").lower() == "true"
+
+# plot ECR v. init for a sweep of amb
+te_amb_step = int(len(te_amb_arr_k) / 10)
+te_amb_short_deg_c = [te_amb_k - celsius_to_kelvin for te_amb_k in te_amb_arr_k][::te_amb_step]
 
 fig, ax = plt.subplots()
 fig.suptitle("UDDS ECR v. Init. and Amb. Temp.")
-for te_amb in te_amb_short:
+for te_amb_deg_c in te_amb_short_deg_c:
     ax.plot(
-        df_res[(df_res["te_amb [*C]"] == te_amb) & (df_res["cycle"] == "udds")]["te_init [*C]"],
-        df_res[(df_res["te_amb [*C]"] == te_amb) & (df_res["cycle"] == "udds")][
+        df_res[(df_res["te_amb [*C]"] == te_amb_deg_c) & (df_res["cycle"] == "udds")][
+            "te_init [*C]"
+        ],
+        df_res[(df_res["te_amb [*C]"] == te_amb_deg_c) & (df_res["cycle"] == "udds")][
             "ECR [kW-hr/100mi]"
         ],
-        label=te_amb,
+        marker=".",
+        label=f"{te_amb_deg_c:.1f}",
     )
 ax.set_xlabel("Cab. and Batt. Init. Temp. [*C]")
 ax.set_ylabel("Energy Consumption Rate [kW-hr/100mi]")
 ax.legend(title="te_amb [*C]")
+plt.tight_layout()
 
-fig0, ax0 = plt.subplots()
-fig0.suptitle("HWFET ECR v. Init. and Amb. Temp.")
-for te_amb in te_amb_short:
-    ax0.plot(
-        df_res[(df_res["te_amb [*C]"] == te_amb) & (df_res["cycle"] == "hwfet")]["te_init [*C]"],
-        df_res[(df_res["te_amb [*C]"] == te_amb) & (df_res["cycle"] == "hwfet")][
+if SAVE_PLOTS:
+    fig.savefig(Path(__file__).parent / "UDDS ECR v. Init. and Amb. Temp.svg")
+
+if SHOW_PLOTS:
+    plt.show()
+
+fig1, ax1 = plt.subplots()
+fig1.suptitle("HWFET ECR v. Init. and Amb. Temp.")
+for te_amb_deg_c in te_amb_short_deg_c:
+    ax1.plot(
+        df_res[(df_res["te_amb [*C]"] == te_amb_deg_c) & (df_res["cycle"] == "hwfet")][
+            "te_init [*C]"
+        ],
+        df_res[(df_res["te_amb [*C]"] == te_amb_deg_c) & (df_res["cycle"] == "hwfet")][
             "ECR [kW-hr/100mi]"
         ],
-        label=te_amb,
+        marker=".",
+        label=f"{te_amb_deg_c:.1f}",
     )
-ax0.set_xlabel("Cab. and Batt. Init. Temp. [*C]")
-ax0.set_ylabel("Energy Consumption Rate [kW-hr/100mi]")
-ax0.legend(title="te_amb [*C]")
+ax1.set_xlabel("Cab. and Batt. Init. Temp. [*C]")
+ax1.set_ylabel("Energy Consumption Rate [kW-hr/100mi]")
+ax1.legend(title="te_amb [*C]")
+plt.tight_layout()
+
+if SAVE_PLOTS:
+    fig1.savefig(Path(__file__).parent / "HWFET ECR v. Init. and Amb. Temp.svg")
+
+if SHOW_PLOTS:
+    plt.show()
+
+te_init_short_deg_c = [te_amb_k - celsius_to_kelvin for te_amb_k in te_amb_arr_k]
+
+
+te_init_step = int(len(te_amb_arr_k) / 10)
+te_init_short_deg_c = [te_init_k - celsius_to_kelvin for te_init_k in te_batt_and_cab_init_arr_k][
+    ::te_init_step
+]
+
+# plot ECR v. amb for a sweep of init
+fig2, ax2 = plt.subplots()
+fig2.suptitle("UDDS ECR v. Amb. and Init. Temp.")
+for te_init_deg_c in te_init_short_deg_c:
+    ax2.plot(
+        df_res[(df_res["te_init [*C]"] == te_init_deg_c) & (df_res["cycle"] == "udds")][
+            "te_amb [*C]"
+        ],
+        df_res[(df_res["te_init [*C]"] == te_init_deg_c) & (df_res["cycle"] == "udds")][
+            "ECR [kW-hr/100mi]"
+        ],
+        marker=".",
+        label=f"{te_amb_deg_c:.1f}",
+    )
+ax2.set_xlabel("Ambient Temp. [*C]")
+ax2.set_ylabel("Energy Consumption Rate [kW-hr/100mi]")
+ax2.legend(title="te_amb [*C]")
+plt.tight_layout()
+
+if SAVE_PLOTS:
+    fig2.savefig(Path(__file__).parent / "UDDS ECR v. Amb. and Init. Temp.svg")
+
+if SHOW_PLOTS:
+    plt.show()
+
+fig3, ax3 = plt.subplots()
+fig3.suptitle("HWFET ECR v. Amb. and Init. Temp.")
+for te_init_deg_c in te_init_short_deg_c:
+    ax3.plot(
+        df_res[(df_res["te_init [*C]"] == te_init_deg_c) & (df_res["cycle"] == "hwfet")][
+            "te_amb [*C]"
+        ],
+        df_res[(df_res["te_init [*C]"] == te_init_deg_c) & (df_res["cycle"] == "hwfet")][
+            "ECR [kW-hr/100mi]"
+        ],
+        marker=".",
+        label=f"{te_amb_deg_c:.1f}",
+    )
+ax3.set_xlabel("Ambient Temp. [*C]")
+ax3.set_ylabel("Energy Consumption Rate [kW-hr/100mi]")
+ax3.legend(title="te_amb [*C]")
+plt.tight_layout()
+
+if SAVE_PLOTS:
+    fig3.savefig(Path(__file__).parent / "HWFET ECR v. Amb. and Init. Temp.svg")
 
 if SHOW_PLOTS:
     plt.show()
