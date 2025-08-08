9f8f677860ff9dc8261eb47080f7b5e0ceecc3a5
Michael O'Keefe <Michael.OKeefe@nrel.gov>
2025-08-08 15:35:23 -0600

    Hook up a checker for the RGWDB powertrain control.
    This method does some sanity checking around the values used for the discharge, charge, and regen buffers and their associated speeds.

--- fastsim-core/src/vehicle/vehicle_model.rs
+++ fastsim-core/src/vehicle/vehicle_model.rs
@@ -253,6 +253,12 @@ impl Init for Vehicle {
         self.pt_type
             .init()
             .map_err(|err| Error::InitError(format_dbg!(err)))?;
+        let mass = self.mass()?.unwrap_or(0.0 * uc::KG);
+        let _ = match &self.pt_type {
+            PowertrainType::HybridElectricVehicle(hev) => hev.check_buffers(mass),
+            PowertrainType::PlugInHybridElectricVehicle(hev) => hev.check_buffers(mass),
+            _ => Ok(()),
+        };
         Ok(())
     }
 }
