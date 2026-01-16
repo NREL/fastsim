833c2123cbe98e5d6a327560aa07d47f5cc84f7b
Robin Steuteville <robin.steuteville@nrel.gov>
2026-01-16 14:34:21 -0700

    adding abs value to check_trace_miss(), getting rid of speed_mph bandaid fix

--- fastsim-core/src/simdrivelabel.rs
+++ fastsim-core/src/simdrivelabel.rs
@@ -79,8 +79,7 @@ pub fn get_0_to_60_time(sd_accel: &mut SimDrive) -> anyhow::Result<f64> {
     // Extract speed values in mph
     let mut speed_mph: Vec<f64> = vec![];
     for s in sd_accel.veh.history.speed_ach.clone() {
-        speed_mph
-            .push((s.get_fresh(|| format_dbg!())?.get::<si::mile_per_hour>() * 10.0).round() / 10.0)
+        speed_mph.push(s.get_fresh(|| format_dbg!())?.get::<si::mile_per_hour>())
     }
 
     // Extract time values in seconds
