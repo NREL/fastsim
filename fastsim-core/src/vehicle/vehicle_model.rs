f9e02ff83cd65703d0460b234036fe0c7116c904
Michael O'Keefe <Michael.OKeefe@nrel.gov>
2026-03-26 09:28:08 -0600

    Test creating and simulating a micro-hybrid
    This creates a vehicle based on the Chrysler Pacifica model but that is a micro-hybrid with start-stop enabled.

--- fastsim-core/src/vehicle/vehicle_model.rs
+++ fastsim-core/src/vehicle/vehicle_model.rs
@@ -945,6 +945,9 @@ impl Default for VehicleState {
 
 #[cfg(test)]
 pub(crate) mod tests {
+    use crate::vehicle::hev::{HEVAuxControls, HEVSimulationParams};
+    use crate::vehicle::powertrain::reversible_energy_storage::EffInterp;
+
     use super::*;
 
     #[allow(dead_code)]
@@ -1103,5 +1106,159 @@ pub(crate) mod tests {
         if time_to_panic {
             panic!()
         }
+    }
+
+    #[test]
+    fn we_can_create_and_simulate_a_micro_hybrid_vehicle() {
+        let res = ReversibleEnergyStorage::new(
+            RESThermalOption::None,             // thrml
+            Option::None,                       // mass
+            Option::None,                       // specific_energy
+            5.0 * uc::KW,                       // pwr_out_max
+            1.0 * uc::KWH,                      // energy_capacity
+            EffInterp::Constant(Interp0D(0.9)), // eff_interp
+            0.0 * uc::R,                        // min_soc
+            1.0 * uc::R,                        // max_soc
+            Option::None,
+        )
+        .unwrap();
+        let fs = FuelStorage::new(
+            2000000.0 * uc::W,
+            1.1 * uc::S,
+            2305080000.0 * uc::J,
+            Option::None,
+            Option::None,
+        )
+        .unwrap();
+        let fc = FuelConverter::new(
+            FuelConverterThermalOption::None, // thrml
+            Option::None,                     // mass
+            Option::None,                     // specific_pwr
+            211088.0 * uc::W,                 // pwr_out_max
+            34604.59016393443 * uc::W,        // pwr_out_max_init
+            6.1 * uc::S,                      // pwr_ramp_lag
+            InterpolatorEnum::new_1d(
+                vec![
+                    0.0, 0.005, 0.015, 0.04, 0.06, 0.1, 0.14, 0.2, 0.4, 0.6, 0.8, 1.0,
+                ]
+                .into(),
+                vec![
+                    0.0,
+                    0.0875106035,
+                    0.143482108,
+                    0.216273855,
+                    0.252599848,
+                    0.301508117,
+                    0.33,
+                    0.34,
+                    0.35,
+                    0.34,
+                    0.32,
+                    0.3,
+                ]
+                .into(),
+                strategy::Linear,
+                Extrapolate::Error,
+            )
+            .unwrap(), // eff_interp_from_pwr_out
+            0.4 * 211088.0 * uc::W,           // pwr_for_peak_eff
+            0.0 * uc::W,                      // pwr_idle_fuel
+            Option::None,
+        )
+        .unwrap();
+        let em = ElectricMachine::new(
+            InterpolatorEnum::new_1d(
+                vec![0.0, 1.0].into(),
+                vec![0.95, 0.95].into(),
+                strategy::Linear,
+                Extrapolate::Error,
+            )
+            .unwrap(), // eff_interp_achieved
+            Option::None, // eff_interp_at_max_input
+            5.0 * uc::KW, // pwr_out_max
+            Option::None, // specific_pwr
+            Option::None, // mass
+            Option::None, // save_interval
+        )
+        .unwrap();
+        let tx = Transmission::new(
+            Option::None,                   // mass
+            InterpolatorEnum::new_0d(0.95), // eff_interp
+            Option::None,                   // save_interval
+        )
+        .unwrap();
+        let ctrl = RESGreedyWithDynamicBuffers::new(
+            Option::None, // speed_soc_disch_buffer
+            Option::None, // speed_soc_disch_buffer_coeff
+            Option::None, // speed_soc_fc_on_buffer
+            Option::None, // speed_soc_fc_on_buffer_coeff
+            Option::None, // speed_soc_regen_buffer
+            Option::None, // speed_soc_regen_buffer_coeff
+            Option::None, // fc_min_time_on
+            Option::None, // speed_fc_forced_on
+            Option::None, // frac_pwr_demand_fc_forced_on
+            Option::None, // frac_of_most_eff_pwr_to_run_fc
+            Option::None, // temp_fc_forced_on
+            Option::None, // temp_fc_allowed_off
+            Option::None, // save_interval
+        )
+        .unwrap();
+        let pt_ctrl = HEVPowertrainControls::RGWDB(Box::new(ctrl));
+        let aux_ctrl = HEVAuxControls::AuxOnResPriority;
+        // TODO: find more reasonable defaults
+        let sim_params = HEVSimulationParams::new(
+            0.05 * uc::R, // res_per_fuel_lim
+            0,            // soc_balance_iter_err
+            false,        // balance_soc
+            false,        // save_soc_bal_iters
+        )
+        .unwrap();
+        let hev = HybridElectricVehicle::new(
+            res,          // res
+            fs,           // fs
+            fc,           // fc
+            em,           // em
+            tx,           // transmission
+            pt_ctrl,      // pt_cntrl
+            aux_ctrl,     // aux_cntrl
+            Option::None, // mass
+            sim_params,   // sim_params
+        )
+        .unwrap();
+        let chassis = Chassis {
+            drag_coef: 0.3303036837542712 * uc::R,
+            frontal_area: 3.05124164 * uc::M2,
+            wheel_rr_coef: 0.0064798953284486704 * uc::R,
+            wheel_inertia: 0.815 * uc::KGM2,
+            num_wheels: 4,
+            wheel_radius: Option::Some(0.36865 * uc::M),
+            tire_code: Option::None,
+            cg_height: 0.53 * uc::M,
+            wheel_fric_coef: 0.8 * uc::R,
+            drive_type: chassis::DriveTypes::FWD,
+            drive_axle_weight_frac: 0.61 * uc::R,
+            wheel_base: 3.08864 * uc::M,
+            mass: Option::None,
+            glider_mass: Option::None,
+            cargo_mass: Option::None,
+        };
+        let boxed_hev = Box::new(hev);
+        let veh = Vehicle::new(
+            String::from("2026 Chrysler Pacifica Select (uHEV Test)"), // name
+            Option::None,                                              // doc
+            2026,                                                      // year
+            PowertrainType::MicroHybrid(boxed_hev),                    // pt_type
+            chassis,                                                   // chassis
+            CabinOption::None,                                         // cabin
+            HVACOption::None,                                          // hvac
+            Option::Some(2154.564 * uc::KG),                           // mass
+            700.0 * uc::W,                                             // pwr_aux_base
+            Option::Some(1),                                           // save_interval
+        )
+        .unwrap();
+        let cyc = crate::drive_cycle::Cycle::from_resource("udds.csv", false).unwrap();
+        let mut sd = crate::simdrive::SimDrive::new(veh, cyc, Default::default());
+        let walk_result = sd.walk();
+        assert!(walk_result.is_ok());
     }
 }
