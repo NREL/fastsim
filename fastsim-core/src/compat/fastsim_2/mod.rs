ffea6acb61c309f4ef2399c0f8703e5b128c660d
Kyle Carow <kylecarow@gmail.com>
2026-08-02 15:37:02 -0600

    fastsim-2 ndarray compat goes through vec to not require matching versions

--- fastsim-core/src/compat/fastsim_2/mod.rs
+++ fastsim-core/src/compat/fastsim_2/mod.rs
@@ -2,7 +2,7 @@ use fastsim_core::traits::SerdeAPI as _;
 
 use super::*;
 
-pub use ::fastsim_2 as fastsim_core;
+pub use fastsim_2 as fastsim_core;
 
 use include_dir::{include_dir, Dir};
 pub const ASSETS_DIR: &'static Dir<'_> =
@@ -122,13 +122,13 @@ impl Vehicle {
             fc_eff_map: self
                 .fc()
                 .map(|fc| match &fc.eff_interp_from_pwr_out {
-                    InterpolatorEnum::Interp1D(interp) => Ok(interp.data.values.clone()),
+                    InterpolatorEnum::Interp1D(interp) => Ok(interp.data.values.to_vec().into()),
                     _ => bail!(format_dbg!(
                         "Only 1-D interpolators can be converted to FASTSim 2"
                     )),
                 })
                 .transpose()?
-                .unwrap_or_else(|| array![0., 0.]),
+                .unwrap_or_else(|| vec![0., 0.].into()),
             fc_eff_map_doc: None,
             fc_eff_type: match &self.pt_type {
                 PowertrainType::ConventionalVehicle(_) => "SI".into(),
@@ -154,13 +154,13 @@ impl Vehicle {
             fc_pwr_out_perc: self
                 .fc()
                 .map(|fc| match &fc.eff_interp_from_pwr_out {
-                    InterpolatorEnum::Interp1D(interp) => Ok(interp.data.grid[0].clone()),
+                    InterpolatorEnum::Interp1D(interp) => Ok(interp.data.grid[0].to_vec().into()),
                     _ => bail!(format_dbg!(
                         "Only 1-D interpolators can be converted to FASTSim 2"
                     )),
                 })
                 .transpose()?
-                .unwrap_or_else(|| array![0., 1.]),
+                .unwrap_or_else(|| vec![0., 1.].into()),
             fc_pwr_out_perc_doc: None,
             fc_sec_to_peak_pwr: self
                 .fc()
@@ -554,8 +554,8 @@ impl TryFrom<&fastsim_core::vehicle::RustVehicle> for BatteryElectricVehicle {
             em: ElectricMachine {
                 state: Default::default(),
                 eff_interp_achieved: InterpolatorEnum::new_1d(
-                    f2veh.mc_pwr_out_perc.clone(),
-                    f2veh.mc_eff_array.clone(),
+                    f2veh.mc_pwr_out_perc.to_vec().into(),
+                    f2veh.mc_eff_array.to_vec().into(),
                     strategy::Linear,
                     Extrapolate::Error,
                 )?,
@@ -568,7 +568,7 @@ impl TryFrom<&fastsim_core::vehicle::RustVehicle> for BatteryElectricVehicle {
                         .zip(f2veh.mc_eff_array.iter())
                         .map(|(x, y)| x / y)
                         .collect(),
-                    f2veh.mc_eff_array.clone(),
+                    f2veh.mc_eff_array.to_vec().into(),
                     strategy::Linear,
                     Extrapolate::Error,
                 )?),
@@ -729,7 +729,7 @@ impl TryFrom<fastsim_core::vehicle::RustVehicle> for FuelConverter {
                     0.0, 0.005, 0.015, 0.04, 0.06, 0.1, 0.14, 0.2, 0.4, 0.6, 0.8, 1.0,
                 ]
                 .into(),
-                f2veh.fc_eff_map.clone().into(),
+                f2veh.fc_eff_map.to_vec().into(),
                 strategy::Linear,
                 Extrapolate::Error,
             )
